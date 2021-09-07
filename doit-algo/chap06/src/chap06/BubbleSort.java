cf9c60a3ad1e8833ae989afaa8cfb9ba497e879f
JunYoung Choi <doforme524@gmail.com>
2021-09-07 10:42:30 +0900

    chap06 bubbleSort

---
+++ doit-algo/chap06/src/chap06/BubbleSort.java
@@ -0,0 +1,57 @@
+package chap06;
+
+import java.util.Scanner;
+
+public class BubbleSort {
+	static void swap(int[] a, int idx1, int idx2) {
+		int t = a[idx1];
+		a[idx1] = a[idx2];
+		a[idx2] = t;
+	}
+	
+	static void bubbleSort(int[] a, int n) {
+		int k = 0;
+		while (k < n - 1) {
+			int last = n - 1;
+			for (int j = n - 1; j > k; j--)
+				if (a[j-1] > a[j]) {
+					swap(a, j-1, j);
+					last = j;
+				}
+			k = last;
+		}
+		for (int i = 0; i < n - 1; i++) {
+			int exchg = 0;
+			for (int j = n - 1; j > i; j--)
+				if (a[j-1] > a[j]) {
+					swap(a, j-1, j);
+					exchg++;
+				}
+			if (exchg == 0) break;
+		}
+			
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner stdIn = new Scanner(System.in);
+		
+		System.out.println("버블 정렬(버전1)");
+		System.out.print("요솟수 : ");
+		int nx = stdIn.nextInt();
+		int[] x = new int[nx];
+		
+		for (int i = 0; i < nx; i++) {
+			System.out.print("x[" + i + "] : ");
+			x[i] = stdIn.nextInt();
+		}
+		
+		bubbleSort(x, nx);
+		
+		System.out.println("오름차순으로 입력했습니다.");
+		for(int i = 0; i < nx; i++) {
+			System.out.println("x[" + i + "]=" + x[i]);
+		}
+		
+	}
+
+}
