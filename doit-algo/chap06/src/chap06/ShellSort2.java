8a587e94bbe31260c7afc5144ec39d9f8d642a8c
JunYoung Choi <doforme524@gmail.com>
2021-09-08 10:59:35 +0900

    chap06 quickSort

--- doit-algo/chap06/src/chap06/ShellSort2.java
+++ doit-algo/chap06/src/chap06/ShellSort2.java
@@ -3,7 +3,21 @@ package chap06;
 import java.util.Scanner;
 
 public class ShellSort2 {
-
+	static void shellSort(int[] a, int n) {
+		int h;
+		for (h = 1; h < n / 9; h = h * 3 + 1) 
+			;
+		
+		for ( ; h > 0; h /= 3)
+			for (int i = h; i < n; i++) {
+				int j;
+				int tmp = a[i];
+				for (j = i - h; j >= 0 && a[j] > tmp; j-= h) {
+					a[j+h] = a[j];
+				}
+				a[j + h] = tmp;
+			}
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner stdIn = new Scanner(System.in);
