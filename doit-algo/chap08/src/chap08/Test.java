d4a244dede3f48cb1143abc89e2cc188183f68ce
JunYoung Choi <doforme524@gmail.com>
2021-09-13 12:56:05 +0900

    chap08 BMmatch

--- doit-algo/chap08/src/chap08/Test.java
+++ doit-algo/chap08/src/chap08/Test.java
@@ -4,8 +4,11 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("junyoung".charAt(1));
-		String name = "junyoung";
+		int[] skip = new int[Character.MAX_VALUE + 1];
+		int i = 0;
+		skip["junyoung".charAt(0)] = 1;
+		System.out.println("junyoung".charAt(0));
+		System.out.println(skip['j']);
 	}
 
 }
