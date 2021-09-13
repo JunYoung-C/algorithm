bf23b72df60b2e5ee303ab29665c6d1ddb6bc642
JunYoung Choi <doforme524@gmail.com>
2021-09-13 11:04:15 +0900

    chap08 kmp

--- doit-algo/chap08/src/chap08/Test.java
+++ doit-algo/chap08/src/chap08/Test.java
@@ -4,10 +4,8 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int len = 5;
-		String s2 = "안녕하살법ㅇㅇ";
-		System.out.print("패턴 : _");
-		System.out.printf(String.format("%%%ds\n", len), s2);
+		System.out.println("junyoung".charAt(1));
+		String name = "junyoung";
 	}
 
 }
