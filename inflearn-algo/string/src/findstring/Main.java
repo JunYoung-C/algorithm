872d986c17eeb17889b749afc7ecec9d75cd71df
JunYoung Choi <doforme524@gmail.com>
2021-09-27 11:23:44 +0900

    chap01 find String

---
+++ inflearn-algo/string/src/findstring/Main.java
@@ -0,0 +1,31 @@
+package findstring;
+
+import java.util.Scanner;
+
+public class Main {
+  public int solution(String str, char t) {
+    int answer = 0;
+    str = str.toUpperCase();
+    t = Character.toUpperCase(t);
+    // System.out.println(str + " " + t);
+//    for (int i = 0; i < str.length(); i++) {
+//      if (str.charAt(i) == t) {
+//        answer++;
+//      }
+//    }
+    for (char c : str.toCharArray()) {
+      if (c == t)
+        answer++;
+    }
+    return answer;
+  }
+
+  public static void main(String[] args) {
+    Main T = new Main();
+    Scanner stdIn = new Scanner(System.in);
+    String str = stdIn.next();
+    char c = stdIn.next().charAt(0);
+    System.out.print(T.solution(str, c));
+  }
+
+}
