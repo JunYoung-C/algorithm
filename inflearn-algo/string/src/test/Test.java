dfcc6922a9ec7fed5e0b5c2a8d7bcd2d1eed0d01
JunYoung Choi <doforme524@gmail.com>
2021-10-02 23:42:17 +0900

    solve 7. 회문문자열

--- inflearn-algo/string/src/test/Test.java
+++ inflearn-algo/string/src/test/Test.java
@@ -5,9 +5,6 @@ import java.util.ArrayList;
 public class Test {
 
   public static void main(String[] args) {
-    StringBuffer sb = new StringBuffer("imgroot");
-    System.out.println(sb.indexOf(Character.toString('i')));
-    System.out.println("hi".charAt(0));
-    
+    System.out.println(5/2);
   }
 }
