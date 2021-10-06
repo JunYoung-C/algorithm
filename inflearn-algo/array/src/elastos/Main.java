8447fb5a145e8eff2b61d909cb43e3b035954e64
JunYoung Choi <doforme524@gmail.com>
2021-10-06 18:08:38 +0900

    solve 5. 소수(에라토스테네스 체)

---
+++ inflearn-algo/array/src/elastos/Main.java
@@ -0,0 +1,35 @@
+package elastos;
+
+import java.util.Scanner;
+
+public class Main {
+  public int solution(int n) {
+    int answer = 0;
+    int[] arr = new int[n + 1];
+    arr[0] = -1;
+    arr[1] = -1;
+    for (int i = 2; i < n + 1; i++) {
+      if (arr[i] == 0) {
+        arr[i] = 1;
+        for (int j = 2 * i; j < n + 1; j += i) {
+          arr[j] = -1;
+        }
+      }
+    }
+
+    for (int i : arr) {
+      if (i == 1) {
+        answer += 1;
+      }
+    }
+
+    return answer;
+  }
+
+  public static void main(String[] args) {
+    Main T = new Main();
+    Scanner stdIn = new Scanner(System.in);
+    int n = stdIn.nextInt();
+    System.out.println(T.solution(n));
+  }
+}
--- inflearn-algo/array/src/test/Test.java
+++ inflearn-algo/array/src/test/Test.java
@@ -1,12 +1,12 @@
 package test;
 
-import java.util.ArrayList;
-import java.util.Scanner;
-
 public class Test {
 
   public static void main(String[] args) {
-    int[] a= {0, 1, 2};
-    System.out.print(a[a.length-1]);
+    boolean[] a= new boolean[5];
+    for (boolean i: a) {
+      System.out.println(i);
+    }
+    
   }
 }
