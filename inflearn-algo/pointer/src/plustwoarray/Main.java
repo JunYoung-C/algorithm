fd1742baefdad4537a4993a5a05ace8d54e58d52
JunYoung Choi <doforme524@gmail.com>
2021-10-10 13:47:01 +0900

    solve 1. 두 배열 합치기

---
+++ inflearn-algo/pointer/src/plustwoarray/Main.java
@@ -0,0 +1,61 @@
+package plustwoarray;
+
+import java.util.Scanner;
+
+public class Main {
+  public int[] solution(int n, int[] nNums, int m, int[] mNums) {
+    int len = n + m;
+    int[] answer = new int[len];
+    
+    int i;
+    int nI = 0, mI = 0;
+    for (i = 0; i < len; i++) {
+      if (nI >= n || mI >= m) {
+        break;
+      }
+      
+      if (nNums[nI] > mNums[mI]) {
+        answer[i] = mNums[mI];
+        mI++;
+      } else {
+        answer[i] = nNums[nI];
+        nI++;
+      }
+//      System.out.print(answer[i] + " ");
+
+    }
+    
+    while (nI <= n-1) {
+      answer[i] = nNums[nI];
+      nI++;
+      i++;
+    } 
+    
+    while (mI <= m-1) {
+      answer[i] = mNums[mI];
+      mI++;
+      i++;
+    }
+    return answer;
+  }
+
+  public static void main(String[] args) {
+    Main T = new Main();
+    Scanner stdIn = new Scanner(System.in);
+    int n = stdIn.nextInt();
+    int[] nNums = new int[n];
+    for (int i = 0; i < n; i++) {
+      nNums[i] = stdIn.nextInt();
+    }
+    
+    int m = stdIn.nextInt();
+    int[] mNums = new int[m];
+    for (int i = 0; i < m; i++) {
+      mNums[i] = stdIn.nextInt();
+    }
+    
+    for (int i: T.solution(n, nNums, m, mNums)) {
+      System.out.print(i + " ");
+    }
+  }
+}
