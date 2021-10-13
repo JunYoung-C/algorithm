4cfe4bcbb4397c682f0ad8a05cff07abd64ac242
JunYoung-C <doforme524@gmail.com>
2021-10-13 18:44:34 +0900

    5. 연속된 자연수의 합(two pointers) - 수학적 접근

--- inflearn-algo/pointer/src/continueint/Main.java
+++ inflearn-algo/pointer/src/continueint/Main.java
@@ -4,23 +4,33 @@ import java.util.Scanner;
 
 public class Main {
   public int solution(int n) {
-    int answer = 0, lt = 1, sum = 0;
-    for (int rt = 1; rt < n; rt++) {
-      sum += rt;
-      if (sum == n) {
+    // 내가 짠 코드
+    // int answer = 0, lt = 1, sum = 0;
+    // for (int rt = 1; rt < n; rt++) {
+    // sum += rt;
+    // if (sum == n) {
+    // answer++;
+    // }
+    // while (sum > n) {
+    // sum -= lt++;
+    // if (sum == n) {
+    // answer++;
+    // break;
+    // }
+    // }
+
+    // 수학적으로 접근
+    int answer = 0, cnt = 1;
+    n--;
+    while (n > 0) {
+      cnt++;
+      n = n - cnt;
+      if (n % cnt == 0) {
         answer++;
       }
-      while (sum > n) {
-        sum -= lt++;
-        if (sum == n) {
-          answer++;
-          break;
-        }
-      }
-
     }
-    
-    // for문의 종료지점을 n/2+1에 도달할때까지로 설정해도 된다. 
+
+    // for문의 종료지점을 n/2+1에 도달할때까지로 설정해도 된다.
     // 그 이후의 값은 연속된 두 자연수로 n이 될 수 없다.
     return answer;
   }
