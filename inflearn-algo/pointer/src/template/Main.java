b503e80907aa23d289ed60c4dcb18b7cf5f64264
JunYoung-C <doforme524@gmail.com>
2021-10-21 14:33:56 +0900

    wrong 5. 쇠막대기

--- inflearn-algo/pointer/src/template/Main.java
+++ inflearn-algo/pointer/src/template/Main.java
@@ -1,46 +1,18 @@
 package template;
 
 import java.util.Scanner;
-import java.util.Stack;
 
 public class Main {
-  public int solution(String str) {
-    int answer = 0;
-    Stack<Integer> stack = new Stack<>();
-    int sum;
-    for(char c : str.toCharArray()) {
-      if (Character.isDigit(c)) {
-        stack.push(c-'0');
+  public String solution() {
+    String answer = "";
 
-      } else {
-        int second = stack.pop();
-        int first = stack.pop();
-        switch (c) {
-          case '+' : {
-            stack.push(first + second);
-            break;
-          } case '-' : {
-            stack.push(first - second);
-            break;
-          } case '*' : {
-            stack.push(first * second);
-            break;
-          } case '/' : {
-            stack.push(first / second);
-            break;
-          }
-        }
-      }
-
-    }
-    answer = stack.pop();
     return answer;
   }
 
   public static void main(String[] args) {
     Main T = new Main();
     Scanner stdIn = new Scanner(System.in);
-    String str = stdIn.next();
-    System.out.println(T.solution(str));
+
+    System.out.println(T.solution());
   }
 }
