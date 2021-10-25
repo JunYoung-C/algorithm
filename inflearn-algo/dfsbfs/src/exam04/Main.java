8eb82568f9c19a18c3fe7f1df2a4dd34e7e717e4
JunYoung-C <doforme524@gmail.com>
2021-10-25 13:57:52 +0900

    solve 5. 동전교환

---
+++ inflearn-algo/dfsbfs/src/exam04/Main.java
@@ -0,0 +1,42 @@
+package exam04;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+  public int bfs(int n, int m, int[] coins) {
+    Queue<Integer> que = new LinkedList<>();
+    int[] ch = new int[m + 1];
+    int L = 1;
+    for (int coin : coins) {
+      que.offer(coin);
+    }
+    
+    while (!que.isEmpty()) {
+      int len = que.size();
+      for (int i = 0; i < len; i++) {
+        int prev = que.poll();
+        for (int coin : coins) {
+          int current = prev + coin;
+          if (current == m) return L + 1;
+          que.offer(current);
+        }
+      }
+      L++;
+    }
+    return -1;
+  }
+
+  public static void main(String[] args) {
+    Main T = new Main();
+    Scanner stdIn = new Scanner(System.in);
+    int n = stdIn.nextInt();
+    int[] coins = new int[n];
+    for (int i = 0; i < n; i++) {
+      coins[i] = stdIn.nextInt();
+    }
+    int m = stdIn.nextInt();
+    System.out.println(T.bfs(n, m, coins));
+  }
+}
---
+++ inflearn-algo/dfsbfs/src/template/Main.java
@@ -0,0 +1,18 @@
+package template;
+
+import java.util.Scanner;
+
+public class Main {
+  static int answer = 0;
+  public void fs() {
+
+  }
+
+  public static void main(String[] args) {
+    Main T = new Main();
+    Scanner stdIn = new Scanner(System.in);
+
+    T.fs();
+    System.out.println(answer);
+  }
+}
