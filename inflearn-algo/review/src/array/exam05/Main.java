9526c51612aed573112868d832f2b0a69d99b5c5
JunYoung-C <doforme524@gmail.com>
2021-10-27 15:49:46 +0900

    redo 5. 소수(에라토스테네스 체)

---
+++ inflearn-algo/review/src/array/exam05/Main.java
@@ -0,0 +1,26 @@
+package array.exam05;
+
+import java.util.Scanner;
+
+public class Main {
+  public int solution(int n) {
+    int answer = 0;
+    int[] arr = new int[n + 1];
+    for (int i = 2; i <= n; i++) {
+      if (arr[i] == 0) {
+        answer++;
+        for (int j = i; j <= n; j+=i) {
+          arr[j] = 1;
+        }
+      }
+    }
+    return answer;
+  }
+
+  public static void main(String[] args) {
+    Main T = new Main();
+    Scanner stdIn = new Scanner(System.in);
+    int n = stdIn.nextInt();
+    System.out.println(T.solution(n));
+  }
+}
