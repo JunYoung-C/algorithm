f23e638c95b3887a06caa6285c543de224c4337e
JunYoung-C <doforme524@gmail.com>
2021-10-30 15:01:25 +0900

    chore : 백준 알고리즘 환경 구축

---
+++ baekjoon/config/src/template/basic/Main.java
@@ -0,0 +1,11 @@
+package template.basic;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner stdIn = new Scanner(System.in);
+    
+    System.out.println();
+  }
+}
