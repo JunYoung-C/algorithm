7ccb3db4e18b8ceeb53f9a32643037ffe6d1a39e
JunYoung-C <doforme524@gmail.com>
2021-11-03 16:55:00 +0900

    solve 두 정수 사이의 합

---
+++ programmers/practice/src/sum/Solution.java
@@ -0,0 +1,20 @@
+package sum;
+
+class Solution {
+  public long solution(int a, int b) {
+      long answer = 0;
+      int start;
+      int end;
+      if (a < b) {
+        start = a;
+        end = b;
+      } else {
+        start = b;
+        end = a;
+      }
+      for (int i = start; i <= end; i++) {
+        answer += i;
+      }
+      return answer;
+  }
+}
