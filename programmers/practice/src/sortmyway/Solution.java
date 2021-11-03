00d2b9c2a6a4c9c64549310c7fde3a43c215d7cb
JunYoung-C <doforme524@gmail.com>
2021-11-03 19:11:33 +0900

    wrong 문자열 내 마음대로 정렬하기

---
+++ programmers/practice/src/sortmyway/Solution.java
@@ -0,0 +1,20 @@
+package sortmyway;
+
+import java.util.Arrays;
+
+class Solution {
+  public String[] solution(String[] strings, int n) {
+    String[] answer = {};
+    for (int i = 0; i < strings.length; i++) {
+      strings[i]= strings[i].charAt(n) + strings[i];
+    }
+
+    Arrays.sort(strings);
+    for (int i = 0; i < strings.length; i++) {
+      strings[i]= strings[i].substring(1);
+    }
+    
+    answer = strings;
+    return answer;
+  }
+}
