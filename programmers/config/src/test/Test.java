e301f88a25b254677a8934c4f57fcabaf6f0046b
JunYoung-C <doforme524@gmail.com>
2021-11-03 22:14:53 +0900

    solve 하샤드 수

--- programmers/config/src/test/Test.java
+++ programmers/config/src/test/Test.java
@@ -4,16 +4,6 @@ import java.util.HashMap;
 
 public class Test {
   public static void main(String[] args) {
-    int[] lower = new int[26];
-    int[] upper = new int[26];
-    for (int i = 0; i < 26; i++) {
-      lower[i] = (char) (i + 'a');
-      upper[i] = (char) (i + 'A');
-    }
     
-
-    for (int i = 0; i < 26; i++) {
-      System.out.println((char)lower[i] + " " + (char)upper[i]);
-    }
   }
 }
