52c1b8314c5d50ae29fc93723b18fb468e309fb5
JunYoung-C <doforme524@gmail.com>
2021-11-03 16:06:06 +0900

    solve 같은 숫자는 싫어

--- programmers/practice/src/getmiddlechar/Solution.java
+++ programmers/practice/src/getmiddlechar/Solution.java
@@ -1,14 +1,20 @@
 package getmiddlechar;
 
-class Solution {
-  public String solution(String s) {
-    String answer = "";
-    int len = s.length();
-    if (len % 2 == 0) {
-      answer = s.substring(len/2-1, len/2+1);
-    } else {
-      answer = s.substring(len/2, len/2+1);
+import java.util.ArrayList;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        int[] answer;
+        int len = arr.length;
+        ArrayList<Integer> filteredArr = new ArrayList<>();
+        filteredArr.add(arr[0]);
+        for (int i = 1; i < len; i++) {
+          if (arr[i] != arr[i-1]) filteredArr.add(arr[i]);
+        }
+        answer= new int[filteredArr.size()];
+        for (int i = 0; i < filteredArr.size(); i++) {
+          answer[i] = filteredArr.get(i);
+        }
+        return answer;
     }
-    return answer;
-  }
 }
