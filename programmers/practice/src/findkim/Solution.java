e7eeb8b41495187a8ba9426adf1c00f7b9d26b3b
JunYoung-C <doforme524@gmail.com>
2021-11-03 22:59:18 +0900

    solve 서울에서 김서방 찾기

---
+++ programmers/practice/src/findkim/Solution.java
@@ -0,0 +1,15 @@
+package findkim;
+
+class Solution {
+  public String solution(String[] seoul) {
+    String answer = "";
+    int len = seoul.length;
+    for (int i = 0; i < len; i++) {
+      if (seoul[i].equals("Kim")) {
+        answer = "김서방은 " + i + "에 있다";
+      }
+    }
+      
+      return answer;
+  }
+}
