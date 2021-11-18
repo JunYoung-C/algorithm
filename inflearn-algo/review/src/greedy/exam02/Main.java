0f1debb55062e7e10068c747990c7a536f0a22e5
JunYoung-C <doforme524@gmail.com>
2021-11-18 21:59:31 +0900

    redo 2. 회의실 배정

--- inflearn-algo/review/src/greedy/exam02/Main.java
+++ inflearn-algo/review/src/greedy/exam02/Main.java
@@ -1,5 +1,57 @@
 package greedy.exam02;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+class Conf implements Comparable<Conf> {
+  int start;
+  int end;
+
+  public Conf(int start, int end) {
+    super();
+    this.start = start;
+    this.end = end;
+  }
+
+  @Override
+  public int compareTo(Conf o) {
+    if (this.end == o.end)
+      return this.start - o.start;
+    return this.end - o.end;
+  }
+
+}
+
+
 public class Main {
 
+  public int solution(int n, ArrayList<Conf> confs) {
+    int answer = 0;
+    int endTime= 0;
+    Collections.sort(confs);
+    for (int i = 0; i < n; i++) {
+      Conf conf = confs.get(i);
+      if (endTime <= conf.start) {
+        endTime = conf.end;
+        answer++;
+//        System.out.println(conf.start + " " + conf.end);
+      }
+    }
+
+    return answer;
+  }
+
+  public static void main(String[] args) {
+    Main T = new Main();
+    Scanner stdIn = new Scanner(System.in);
+    int n = stdIn.nextInt();
+    ArrayList<Conf> confs = new ArrayList<>();
+    for (int i = 0; i < n; i++) {
+      int s = stdIn.nextInt();
+      int e = stdIn.nextInt();
+      confs.add(new Conf(s, e));
+    }
+    System.out.println(T.solution(n, confs));
+  }
 }
