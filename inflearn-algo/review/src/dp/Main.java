26490a51e983cbef9137069737dd8c8483dc1bea
JunYoung-C <doforme524@gmail.com>
2021-11-22 15:14:45 +0900

    redo 1. 계단오르기

---
+++ inflearn-algo/review/src/dp/Main.java
@@ -0,0 +1,23 @@
+package dp;
+
+import java.util.Scanner;
+
+public class Main {
+
+  public int solution(int n) {
+    int[] dp = new int[n+1];
+    dp[1] = 1;
+    dp[2] = 2;
+    for (int i = 3; i <= n; i++) {
+      dp[i] = dp[i-2] + dp[i-1];
+    }
+    return dp[n];
+  }
+
+  public static void main(String[] args) {
+    Main T = new Main();
+    Scanner stdIn = new Scanner(System.in);
+    int n = stdIn.nextInt();
+    System.out.println(T.solution(n));
+  }
+}
