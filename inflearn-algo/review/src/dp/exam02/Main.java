54e6e8455f341c852ec9f559170ce8b28907491a
JunYoung-C <doforme524@gmail.com>
2021-11-22 15:16:19 +0900

    redo 2. 돌다리 건너기

--- inflearn-algo/review/src/dp/exam02/Main.java
+++ inflearn-algo/review/src/dp/exam02/Main.java
@@ -1,17 +1,17 @@
-package dp;
+package dp.exam02;
 
 import java.util.Scanner;
 
 public class Main {
 
   public int solution(int n) {
-    int[] dp = new int[n+1];
+    int[] dp = new int[n+2];
     dp[1] = 1;
     dp[2] = 2;
-    for (int i = 3; i <= n; i++) {
+    for (int i = 3; i <= n+1; i++) {
       dp[i] = dp[i-2] + dp[i-1];
     }
-    return dp[n];
+    return dp[n+1];
   }
 
   public static void main(String[] args) {
