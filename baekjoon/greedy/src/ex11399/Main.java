591e922660a8365ed747e12ad6b40cb7d129750e
JunYoung-C <doforme524@gmail.com>
2021-11-26 14:52:23 +0900

    solve 11399

--- baekjoon/greedy/src/ex11399/Main.java
+++ baekjoon/greedy/src/ex11399/Main.java
@@ -1,15 +1,19 @@
-package template.buffer;
+package ex11399;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 
 public class Main {
-  public String solution() {
-    String answer = "";
-    
+  public int solution(int n, int[] arr) {
+    int answer = 0;
+    Arrays.sort(arr);
+    for (int i = n; i >= 1; i--) {
+      answer += arr[n-i] * i;
+    }
     return answer;
   }
   
@@ -17,8 +21,16 @@ public class Main {
     Main T = new Main();
     BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter print = new BufferedWriter(new OutputStreamWriter(System.out));
+    String strN = input.readLine();
+    int n = Integer.parseInt(strN);
+    
+    int[] arr = new int[n];
+    String[] strArr =input.readLine().split(" ");
+    for (int i = 0; i < n; i++) {
+      arr[i] = Integer.parseInt(strArr[i]);
+    }
 
-    print.write(T.solution()+ "");
+    print.write(T.solution(n, arr) + "");
     print.flush();
     print.close();
   }
