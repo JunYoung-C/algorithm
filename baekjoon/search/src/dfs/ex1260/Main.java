8ba5f680888ea286a8c6be7712af43ea18b8aaae
JunYoung-C <doforme524@gmail.com>
2021-12-31 13:08:40 +0900

    redo 1260 dfs와 bfs

--- baekjoon/search/src/dfs/ex1260/Main.java
+++ baekjoon/search/src/dfs/ex1260/Main.java
@@ -3,6 +3,8 @@ package dfs.ex1260;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -11,46 +13,43 @@ public class Main {
   boolean[] isVisited;
 
   // n = 정점의 개수, m = 간선의 개수
-  public void solution(int n, int m, int start, int[][] graph) {
+  public void solution(int n, int m, int start, ArrayList<ArrayList<Integer>> graph) {
     isVisited = new boolean[n + 1];
-    dfs(n, m, start, graph);
+    dfs(n, start, graph);
     System.out.println();
     isVisited = new boolean[n + 1];
-    bfs(n, m, start, graph);
+    bfs(n, start, graph);
   }
 
-  void dfs(int n, int m, int now, int[][] graph) {
+  void dfs(int n, int now, ArrayList<ArrayList<Integer>> graph) {
     isVisited[now] = true;
     System.out.print(now + " ");
-    
-    for (int node = 1; node <= n; node++) {
-      if (!isVisited[node] && graph[now][node] == 1) {
-        dfs(n, m, node, graph);
+
+    for (int LinkedNode : graph.get(now)) {
+      if (!isVisited[LinkedNode]) {
+        dfs(n, LinkedNode, graph);
       }
     }
   }
 
-  void bfs(int n, int m, int start, int[][] graph) {
-    int L = 1;
+  void bfs(int n, int start, ArrayList<ArrayList<Integer>> graph) {
+    int L = 0;
     Queue<Integer> que = new LinkedList<>();
     que.offer(start);
-    isVisited[start] = true;
-    
+
     while (!que.isEmpty()) {
       int now = que.poll();
+      if (isVisited[now]) {
+        continue;
+      }
+      isVisited[now] = true;
       System.out.print(now + " ");
-      
-      for (int node = 1; node <= n; node++) {
-        if (!isVisited[node] && graph[now][node] == 1) {
-          que.offer(node);
-          isVisited[node] = true;
+      for (int LinkedNode : graph.get(now)) {
+        if (!isVisited[LinkedNode]) {
+          que.offer(LinkedNode);
         }
       }
-      
-      L++;
-      if (L > n) break;
     }
-
   }
 
   public static void main(String[] args) throws IOException {
@@ -63,14 +62,14 @@ public class Main {
     int v = Integer.parseInt(st.nextToken()); // 탐색을 시작할 정점의 번호
 
     // 배열
-    int[][] graph = new int[n + 1][n + 1];
-    for (int i = 0; i < m; i++) {
-      st = new StringTokenizer(br.readLine());
-      int a = Integer.parseInt(st.nextToken());
-      int b = Integer.parseInt(st.nextToken());
-      graph[a][b] = 1;
-      graph[b][a] = 1;
-    }
+    // int[][] graph = new int[n + 1][n + 1];
+    // for (int i = 0; i < m; i++) {
+    // st = new StringTokenizer(br.readLine());
+    // int a = Integer.parseInt(st.nextToken());
+    // int b = Integer.parseInt(st.nextToken());
+    // graph[a][b] = 1;
+    // graph[b][a] = 1;
+    // }
 
     // for (int[] nodes : graph) {
     // for (int node : nodes) {
@@ -80,18 +79,22 @@ public class Main {
     // }
 
     // arrayList
-    // ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-    // for (int i = 0; i <= n; i++) {
-    // graph.add(new ArrayList<>());
-    // }
-    //
-    // for (int i = 0; i < m; i++) {
-    // st = new StringTokenizer(br.readLine());
-    // int a = Integer.parseInt(st.nextToken());
-    // int b = Integer.parseInt(st.nextToken());
-    // graph.get(a).add(b);
-    // graph.get(b).add(a);
-    // }
+    ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+    for (int i = 0; i <= n; i++) {
+      graph.add(new ArrayList<>());
+    }
+
+    for (int i = 0; i < m; i++) {
+      st = new StringTokenizer(br.readLine());
+      int a = Integer.parseInt(st.nextToken());
+      int b = Integer.parseInt(st.nextToken());
+      graph.get(a).add(b);
+      graph.get(b).add(a);
+    }
+    
+    for (int i = 1; i <= n; i++) {
+      Collections.sort(graph.get(i));
+    }
 
     // for (ArrayList<Integer> nodes : graph) {
     // for (int node : nodes) {
