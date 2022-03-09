cacbd287e24fa0515f3a61d7951357c929538412
JunYoung-C <doforme524@gmail.com>
2022-03-09 21:21:31 +0900

    wrong lv3. 표 편집
    이전에는 char[]에 O, X를 담아서 사용했다면, 이번에는 이전 노드, 다음 노드, 인덱스, O/X 정보가 담긴 Node 배열을 활용하여 코드를 전개했다.효율성 면에서 향상되었지만, 효율성 테스트 케이스 한개가 시간 초과했다.

--- intellij/src/programmers/exam/kakao/intern2021/edittable/Solution.java
+++ intellij/src/programmers/exam/kakao/intern2021/edittable/Solution.java
@@ -3,69 +3,133 @@ package programmers.exam.kakao.intern2021.edittable;
 import java.util.Scanner;
 import java.util.Stack;
 
+class Node {
+    Node prevNode;
+    Node nextNode;
+    int index;
+    char status;
+
+    public Node(Node prevNode, int index) {
+        this.prevNode = prevNode;
+        this.nextNode = null;
+        this.index = index;
+        this.status = 'O';
+    }
+}
+
 class Solution {
     public String solution(int n, int k, String[] cmd) {
-        char[] table = new char[n];
+        Node[] table = new Node[n];
         Stack<Integer> deletedIndexStack = new Stack<>();
-        for (int i = 0; i < n; i++) {
-            table[i] = 'O';
+        // 0번째 노드 : prevNode = null, n - 1번째 노드 : nextNode = null
+        table[0] = new Node(null, 0);
+        for (int i = 1; i < n; i++) {
+            table[i] = new Node(table[i - 1], i);
+            table[i - 1].nextNode = table[i];
         }
 
-        int nowIndex = k;
+        Node nowNode = table[k];
+        Node prevNode, nextNode;
         for (String command : cmd) {
             if (command.equals("C")) {
-                table[nowIndex] = 'X';
-                deletedIndexStack.push(nowIndex);
+                nowNode.status = 'X';
+                deletedIndexStack.push(nowNode.index);
 
-                while (++nowIndex < n && table[nowIndex] == 'X') {}
+                if (nowNode.nextNode == null) {
+                    prevNode = nowNode.prevNode;
+                    prevNode.nextNode = null;
+                    nowNode.prevNode = null;
 
-                if (nowIndex >= n) {
-                    while (table[--nowIndex] == 'X') {}
+                    nowNode = prevNode;
+                } else if (nowNode.prevNode == null) {
+                    nextNode = nowNode.nextNode;
+                    nextNode.prevNode = null;
+                    nowNode.nextNode = null;
+
+                    nowNode = nextNode;
+                } else {
+                    prevNode = nowNode.prevNode;
+                    nextNode = nowNode.nextNode;
+                    prevNode.nextNode = nextNode;
+                    nextNode.prevNode = prevNode;
+                    nowNode.prevNode = nowNode.nextNode = null;
+
+                    nowNode = nextNode;
                 }
-
             } else if (command.equals("Z")) {
                 int deletedIndex = deletedIndexStack.pop();
-                table[deletedIndex] = 'O';
+                table[deletedIndex].status = 'O';
+                nextNode = null;
+                prevNode = null;
+
+                // nextNode 찾기
+                for (int rt = deletedIndex + 1; rt < n; rt++) {
+                    if (table[rt].status == 'O') {
+                        nextNode = table[rt];
+                        break;
+                    }
+                }
+
+                // prevNode 찾기
+                for (int lt = deletedIndex - 1; lt >= 0; lt--) {
+                    if (table[lt].status == 'O') {
+                        prevNode = table[lt];
+                        break;
+                    }
+                }
+
+                table[deletedIndex].prevNode = prevNode;
+                table[deletedIndex].nextNode = nextNode;
+                if (prevNode != null) {
+                    prevNode.nextNode = table[deletedIndex];
+                }
+                if (nextNode != null) {
+                    nextNode.prevNode = table[deletedIndex];
+                }
             } else { // D X, U X
                 String[] moveCmd = command.split(" ");
 
-                nowIndex = moveAndReturnIndex(moveCmd[0], Integer.parseInt(moveCmd[1]), nowIndex, n, table);
-//                System.out.println(nowIndex);
+                nowNode = moveAndReturnNowNode(moveCmd[0], Integer.parseInt(moveCmd[1]), nowNode, n, table);
+//                System.out.println(nowNode.index);
             }
         }
-        return String.valueOf(table);
+
+        StringBuilder sb = new StringBuilder();
+        for (Node node : table) {
+            sb.append(node.status);
+        }
+        return sb.toString();
     }
 
-    private int moveAndReturnIndex(String direction, int count, int nowIndex, int len, char[] table) {
+    private Node moveAndReturnNowNode(String direction, int count, Node nowNode, int len, Node[] table) {
         int moveCnt = 0;
+
         if (direction.equals("D")) { // 인덱스 증가
             while (moveCnt != count) {
-                while (table[++nowIndex] == 'X') {
-                }
+                nowNode = nowNode.nextNode;
                 moveCnt++;
             }
         } else { // 인덱스 감소
             while (moveCnt != count) {
-                while (table[--nowIndex] == 'X') {
-                }
+                nowNode = nowNode.prevNode;
                 moveCnt++;
             }
         }
 
-        return nowIndex;
+        return nowNode;
     }
 
     public static void main(String[] args) {
         Solution T = new Solution();
         Scanner stdIn = new Scanner(System.in);
 
-//        int n = 8;
-//        int k = 2;
-//        String[] cmd = {"D 2", "C", "U 3", "C", "D 4", "C", "U 2", "Z", "Z"};
-
         int n = 8;
         int k = 2;
-        String[] cmd = {"D 2","C","U 3","C","D 4","C","U 2","Z","Z","U 1","C"};
+        String[] cmd = {"D 2", "C", "U 3", "C", "D 4", "C", "U 2", "Z", "Z"};
+
+//        int n = 8;
+//        int k = 2;
+//        String[] cmd = {"D 2","C","U 3","C","D 4","C","U 2","Z","Z","U 1","C"};
         System.out.println(T.solution(n, k, cmd));
     }
 }
