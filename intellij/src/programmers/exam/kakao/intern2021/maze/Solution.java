58f960138dbf12fe36db9398f9141bf35d3adbea
JunYoung-C <doforme524@gmail.com>
2022-04-22 15:28:45 +0900

    chore: 파일 위치 변경

--- intellij/src/programmers/exam/kakao/intern2021/maze/Solution.java
+++ intellij/src/programmers/exam/kakao/intern2021/maze/Solution.java
@@ -2,13 +2,29 @@ package programmers.exam.kakao.intern2021.maze;
 
 import java.util.*;
 
+class Node implements Comparable<Node> {
+    int index;
+    int distance;
+    int[] visitCnt;
+
+    public static Node create(int index, int distance, int n) {
+        Node node = new Node();
+        node.index = index;
+        node.distance = distance;
+        node.visitCnt = new int[n + 1];
+        return node;
+    }
+
+    @Override
+    public int compareTo(Node o) {
+        return this.distance - o.distance;
+    }
+}
+
 class Solution {
-    static int[][] graph;
-    static int[] visitCnt;
-    static int minDis = Integer.MAX_VALUE;
     public int solution(int n, int start, int end, int[][] roads, int[] traps) {
-        graph = new int[n + 1][n + 1];
-        visitCnt = new int[n + 1];
+        int answer = 0;
+        int[][] graph = new int[n + 1][n + 1];
         for (int i = 0; i < n + 1; i++) {
             Arrays.fill(graph[i], -1);
         }
@@ -20,44 +36,47 @@ class Solution {
             graph[a][b] = c;
         }
 
-        visitCnt[start] = 1;
-        dfs(0, start, end, traps, n);
-
+        answer = bfs(n, start, end, traps, graph);
 //        for (int i = 0; i < n + 1; i++) {
 //            for (int j = 0; j < n + 1; j++) {
 //                System.out.print(graph[i][j] + " ");
 //            }
 //            System.out.println();
 //        }
-        return minDis;
+        return answer;
     }
 
-    void dfs(int distance, int now, int end, int[] traps, int n) {
-        if (minDis <= distance) {
-            return;
-        }
+    int bfs(int n, int start, int end, int[] traps, int[][] graph) {
+        PriorityQueue<Node> pQ = new PriorityQueue<>();
+        int[] minDistances = new int[n + 1];
+        Arrays.fill(minDistances, Integer.MAX_VALUE);
+        pQ.offer(Node.create(1, 0, 0));
+        minDistances[start] = 0;
 
-        if (now == end) {
-            minDis = Math.min(minDis, distance);
-            return;
-        } else {
-            for (int next = 1; next <= n; next++) {
-                int cost = graph[now][next];
-                if (cost == -1 || visitCnt[next] >= 2) {
+        while (!pQ.isEmpty()) {
+            Node now = pQ.poll();
+            now.visitCnt[now.index] += 1;
+
+            if (minDistances[now.index] < now.distance) {
+                continue;
+            }
+
+            for (int nextIndex = 1; nextIndex <= n; nextIndex++) {
+                int cost = graph[now.index][nextIndex];
+                if (now.visitCnt[nextIndex] >= 2) {
                     continue;
                 }
 
-                visitCnt[next] += 1;
-                if (isTrap(traps, next)) {
-                    changeDirection(n, next);
-                    dfs(distance + cost, next, end, traps, n);
-                    changeDirection(n, next);
+                if (cost != -1) {
+
                 } else {
-                    dfs(distance + cost, next, end, traps, n);
+                    
                 }
-                visitCnt[next] -= 1;
+
             }
         }
+
+        return minDistances[end];
     }
 
     boolean isTrap(int[] arr, int num) {
@@ -69,7 +88,7 @@ class Solution {
         return false;
     }
 
-    void changeDirection(int n, int trap) {
+    void ChangeDirection(int n, int trap, int[][] graph) {
         int tmp;
         for (int i = 1; i <= n; i++) {
             tmp = graph[trap][i];
@@ -82,17 +101,17 @@ class Solution {
         Solution T = new Solution();
         Scanner stdIn = new Scanner(System.in);
 
-        int n = 3;
-        int start = 1;
-        int end = 3;
-        int[][] roads = {{1, 2, 2}, {3, 2, 3}};
-        int[] traps = {2};
-
-//        int n = 4;
+//        int n = 3;
 //        int start = 1;
-//        int end = 4;
-//        int[][] roads = {{1, 2, 1}, {3, 2,1}, {2, 4, 1}};
-//        int[] traps = {2, 3};
+//        int end = 3;
+//        int[][] roads = {{1, 2, 2}, {3, 2, 3}};
+//        int[] traps = {2};
+
+        int n = 4;
+        int start = 1;
+        int end = 4;
+        int[][] roads = {{1, 2, 1}, {3, 2, 1}, {2, 4, 1}};
+        int[] traps = {2, 3};
 
         System.out.println(T.solution(n, start, end, roads, traps));
     }
