60f1df64b4d2d508116b0b96cf7d5988538c04f8
JunYoung-C <doforme524@gmail.com>
2022-04-23 15:24:42 +0900

    success: 탐색 ex11724 연결 요소의 개수

---
+++ intellij/src/backjoon/bfsdfs/ex11724/Main.java
@@ -0,0 +1,61 @@
+package backjoon.bfsdfs.ex11724;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+    boolean[] isVisited;
+
+    public int solution(int nodeCount, int edgeCount, ArrayList<ArrayList<Integer>> graph) {
+        int answer = 0;
+        isVisited = new boolean[nodeCount + 1];
+
+        for (int node = 1; node <= nodeCount; node++) {
+            if (!isVisited[node]) {
+                answer++;
+                dfs(node, graph);
+            }
+        }
+
+        return answer;
+    }
+
+    private void dfs(int now, ArrayList<ArrayList<Integer>> graph) {
+        isVisited[now] = true;
+
+        for (int next : graph.get(now)) {
+            if (!isVisited[next]) {
+                dfs(next, graph);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main T = new Main();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int nodeCount = Integer.parseInt(st.nextToken());
+        int edgeCount = Integer.parseInt(st.nextToken());
+
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i <= nodeCount; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < edgeCount; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            graph.get(a).add(b);
+            graph.get(b).add(a);
+        }
+
+        bw.write(T.solution(nodeCount, edgeCount, graph)+ "");
+        bw.flush();
+        bw.close();
+    }
+}
