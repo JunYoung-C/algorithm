5dd96bd4cfbcb8064c682209e725357ef5cd79d9
JunYoung-C <doforme524@gmail.com>
2022-05-05 14:25:52 +0900

    fail: LCA ex11438 LCA2

---
+++ intellij/src/backjoon/tree/ex11438/Main.java
@@ -0,0 +1,163 @@
+package backjoon.tree.ex11438;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static ArrayList<ArrayList<Integer>> tree = new ArrayList<>();
+    static int[][] p;
+    static int[] depths;
+
+    public String solution() {
+        String answer = "";
+
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main T = new Main();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int nodeCount = Integer.parseInt(br.readLine());
+        for (int i = 0; i <= nodeCount; i++) {
+            tree.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < nodeCount - 1; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            tree.get(a).add(b);
+            tree.get(b).add(a);
+        }
+
+        depths = new int[nodeCount + 1];
+        int depth = findDepthAndInitDepths(nodeCount);
+        int k = findExponent(depth);
+
+        p = new int[k + 1][nodeCount + 1];
+        initArrayP(nodeCount, k);
+
+        int findCount = Integer.parseInt(br.readLine());
+        for (int i = 0; i < findCount; i++) {
+            st = new StringTokenizer(br.readLine());
+            int num1 = Integer.parseInt(st.nextToken());
+            int num2 = Integer.parseInt(st.nextToken());
+
+            if (depths[num1] > depths[num2]) { // num1가 더 깊으므로 num1의 깊이를 낮춘다.
+                num1 = findSameDepthParent(depths[num1] - depths[num2], num1);
+            } else if (depths[num1] < depths[num2]) { // num2가 더 깊으므로 num2의 깊이를 낮춘다.
+                num2 = findSameDepthParent(depths[num2] - depths[num1], num2);
+            }
+
+            bw.write(findLCA(num1, num2) + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+
+    private static int findLCA(int num1, int num2) {
+        if (num1 == num2) {
+            return num1;
+        } else if (p[0][num1] == p[0][num2]) {
+            return p[0][num1];
+        }
+
+        int commonAncestorExponent = findCAExponent(num1, num2);
+        num1 = p[commonAncestorExponent - 1][num1];
+        num2 = p[commonAncestorExponent - 1][num2];
+        return findLCA(num1, num2);
+    }
+
+    private static int findCAExponent(int num1, int num2) {
+        int commonAncestorExponent = 0;
+
+        while (p[commonAncestorExponent][num1] != p[commonAncestorExponent][num2]) {
+            commonAncestorExponent++;
+        }
+
+        return commonAncestorExponent;
+    }
+
+    private static int findSameDepthParent(int depthDif, int child) {
+        int k = findExponent(depthDif);
+        int reducedDepthDif = (int) Math.pow(2, k);
+
+        child = p[k][child];
+        if (reducedDepthDif == depthDif) {
+            return child;
+        }
+
+        return findSameDepthParent(reducedDepthDif, child);
+    }
+
+    private static void initArrayP(int nodeCount, int k) {
+        Queue<Integer> que = new LinkedList<>();
+        boolean[] isVisited = new boolean[nodeCount + 1];
+        que.offer(1);
+        isVisited[1] = true;
+
+        while (!que.isEmpty()) {
+            int parent = que.poll();
+
+            for (int child : tree.get(parent)) {
+                if (!isVisited[child]) {
+                    que.offer(child);
+                    isVisited[child] = true;
+                    p[0][child] = parent;
+                }
+            }
+        }
+
+        for (int exponent = 1; exponent <= k; exponent++) {
+            for (int n = 1; n <= nodeCount; n++) {
+                p[exponent][n] = p[exponent - 1][p[exponent - 1][n]];
+
+            }
+        }
+    }
+
+    private static int findExponent(int depth) {
+        int k = 0;
+        while (Math.pow(2, k) < depth) {
+            k++;
+        }
+        return k;
+    }
+
+    private static int findDepthAndInitDepths(int nodeCount) {
+        Queue<Integer> que = new LinkedList<>();
+        boolean[] isVisited = new boolean[nodeCount + 1];
+        que.offer(1);
+        isVisited[1] = true;
+        int depth = 0;
+
+        while (!que.isEmpty()) {
+            int len = que.size();
+
+            for (int i = 0; i < len; i++) {
+                int now = que.poll();
+                depths[now] = depth;
+
+                for (int next : tree.get(now)) {
+                    if (!isVisited[next]) {
+                        que.offer(next);
+                        isVisited[next] = true;
+                    }
+                }
+            }
+
+            depth++;
+        }
+
+        return depth - 1;
+    }
+}
