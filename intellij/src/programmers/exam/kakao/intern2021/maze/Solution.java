c121768f63fcac7b25a45d637d035c5f0caa652a
JunYoung-C <doforme524@gmail.com>
2022-05-06 22:44:05 +0900

    fail: 2021 카카오 인턴십 - 미로 찾기
    주석 처리한 풀이가 맞는지 확신이 없어서 풀이를 보았다. 우선순위 큐를 사용하는 것은 동일하지만, 모든 트랩을 고려한 그래프가 1000 * 1024이므로, 모두 고려하여 로직을 전개하였다

--- intellij/src/programmers/exam/kakao/intern2021/maze/Solution.java
+++ intellij/src/programmers/exam/kakao/intern2021/maze/Solution.java
@@ -2,98 +2,74 @@ package programmers.exam.kakao.intern2021.maze;
 
 import java.util.*;
 
-class Node implements Comparable<Node> {
-    int index;
-    int distance;
-    int[] visitCnt;
-
-    public static Node create(int index, int distance, int n) {
-        Node node = new Node();
-        node.index = index;
-        node.distance = distance;
-        node.visitCnt = new int[n + 1];
-        return node;
-    }
-
-    @Override
-    public int compareTo(Node o) {
-        return this.distance - o.distance;
-    }
-}
-
 class Solution {
+    static int[][] graph;
+
     public int solution(int n, int start, int end, int[][] roads, int[] traps) {
         int answer = 0;
-        int[][] graph = new int[n + 1][n + 1];
-        for (int i = 0; i < n + 1; i++) {
-            Arrays.fill(graph[i], -1);
+        initGraph(n, roads);
+
+        PriorityQueue<Node> edges = new PriorityQueue<>();
+        for (int next = 1; next <= n; next++) {
+            if (graph[start][next] != Integer.MAX_VALUE) {
+                edges.offer(new Node(next, graph[start][next], new boolean[n + 1]));
+            }
         }
 
-        for (int[] road : roads) {
-            int a = road[0];
-            int b = road[1];
-            int c = road[2];
-            graph[a][b] = c;
+        // end에 도달하면 종료
+        while (!edges.isEmpty()) {
+            Node currentNode = edges.poll();
+            boolean visitTrapNode = visitTrapNode(currentNode.index, traps); // 현재 노드가 트랩인지 확인
+            boolean[] isTrapped = currentNode.isTrapped.clone();
+
+            // que에 넣기 전에 현재 노드가 trap인지 계산하고 넣어야함
+            if (visitTrapNode) {
+                isTrapped[currentNode.index] = !isTrapped[currentNode.index];
+                // next가 트랩 on인 경우에는 현재 노드 -> next가 가능한 경우를 고려한다.
+                // next가 트랩 off, 트랩x인 경우에는 next -> 현재 노드가 가능한 경우를 고려해야 한다.
+            } else {
+                // next가 트랩 on인 경우에는 next -> 현재노드가 가능한 경우를 고려한다.
+                // next가 트랩 off, 트랩x인 경우에는 현재 노드 -> next가 가능한 경우를 고려해야 한다.
+
+            }
         }
 
-        answer = bfs(n, start, end, traps, graph);
-//        for (int i = 0; i < n + 1; i++) {
-//            for (int j = 0; j < n + 1; j++) {
-//                System.out.print(graph[i][j] + " ");
-//            }
-//            System.out.println();
-//        }
         return answer;
     }
 
-    int bfs(int n, int start, int end, int[] traps, int[][] graph) {
-        PriorityQueue<Node> pQ = new PriorityQueue<>();
-        int[] minDistances = new int[n + 1];
-        Arrays.fill(minDistances, Integer.MAX_VALUE);
-        pQ.offer(Node.create(1, 0, 0));
-        minDistances[start] = 0;
-
-        while (!pQ.isEmpty()) {
-            Node now = pQ.poll();
-            now.visitCnt[now.index] += 1;
-
-            if (minDistances[now.index] < now.distance) {
-                continue;
-            }
-
-            for (int nextIndex = 1; nextIndex <= n; nextIndex++) {
-                int cost = graph[now.index][nextIndex];
-                if (now.visitCnt[nextIndex] >= 2) {
-                    continue;
-                }
-
-                if (cost != -1) {
-
-                } else {
-                    
-                }
-
-            }
-        }
-
-        return minDistances[end];
-    }
-
-    boolean isTrap(int[] arr, int num) {
-        for (int i : arr) {
-            if (i == num) {
+    private boolean visitTrapNode(int index, int[] traps) {
+        for (int trap : traps) {
+            if (index == trap) {
                 return true;
             }
         }
         return false;
     }
 
-    void ChangeDirection(int n, int trap, int[][] graph) {
-        int tmp;
-        for (int i = 1; i <= n; i++) {
-            tmp = graph[trap][i];
-            graph[trap][i] = graph[i][trap];
-            graph[i][trap] = tmp;
+    private void initGraph(int n, int[][] roads) {
+        graph = new int[n + 1][n + 1];
+        for (int row = 1; row <= n; row++) {
+            Arrays.fill(graph[row], Integer.MAX_VALUE);
+        }
+        for (int[] road : roads) {
+            graph[road[0]][road[1]] = Math.min(graph[road[0]][road[1]], road[2]);
+        }
+    }
+
+    class Node implements Comparable<Node> {
+        int index;
+        int accumulateCost;
+        boolean[] isTrapped;
+
+        public Node(int index, int accumulateCost, boolean[] isTrapped) {
+            this.index = index;
+            this.accumulateCost = accumulateCost;
+            this.isTrapped = isTrapped;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return this.accumulateCost - o.accumulateCost;
         }
     }
 
@@ -112,7 +88,6 @@ class Solution {
         int end = 4;
         int[][] roads = {{1, 2, 1}, {3, 2, 1}, {2, 4, 1}};
         int[] traps = {2, 3};
-
         System.out.println(T.solution(n, start, end, roads, traps));
     }
 }
