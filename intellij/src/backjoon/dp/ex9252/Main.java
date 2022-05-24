89e8fffc2ad273a461123e41d2dd14d266eb084a
JunYoung-C <doforme524@gmail.com>
2022-05-24 13:58:12 +0900

    *success: dp ex9252 LCS2 이전 풀이에서는 일차원 dp로 값을 구하고, dp[i] > dp[i - 1]인 경우에 해당 인덱스의 문자를 포함하는 방식으로 했지만 틀렸다. 해당 인덱스의 문자가 서로 같은지 비교하는 수식이 없어서 그런 것 같다. 이번 풀이에서는 이차원 dp로 동일한 알고리즘으로 값을 구한 후에 공통 문자를 재귀 함수로 찾는 방식을 익힐 수 있었다.

--- intellij/src/backjoon/dp/ex9252/Main.java
+++ intellij/src/backjoon/dp/ex9252/Main.java
@@ -3,44 +3,52 @@ package backjoon.dp.ex9252;
 import java.io.*;
 
 public class Main {
-    public String[] solution(String str1, String str2) {
-        String[] answer = new String[2];
+    StringBuilder sb = new StringBuilder();
+    int[][] dp;
+    public void solution(String str1, String str2) {
         int len1 = str1.length();
-        int[] dp = new int[len1 + 1];
+        int len2 = str2.length();
+        dp = new int[len1 + 1][len2 + 1];
 
-        for (char c : str2.toCharArray()) {
-            for (int i = 0; i < len1; i++) {
-                if (str1.charAt(i) == c) {
-                    dp[i + 1] = dp[i] + 1;
+        for (int i = 1; i <= len1; i++) {
+            for (int j = 1; j <= len2; j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
                 } else {
-                    dp[i + 1] = Math.max(dp[i], dp[i + 1]);
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
 
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < len1; i++) {
-            if (dp[i + 1] > dp[i]) {
-                sb.append(str1.charAt(i));
-            }
+        System.out.println(dp[len1][len2]);
+        getText(len1, len2, str1, str2);
+        System.out.println(sb.reverse());
+    }
+
+    private void getText(int r, int c, String str1, String str2) {
+        if (r == 0 || c == 0) {
+            return;
         }
 
-        answer[0] = String.valueOf(dp[len1]);
-        answer[1] = sb.toString();
-        return answer;
+        if (str1.charAt(r - 1) == str2.charAt(c - 1)) {
+            sb.append(str1.charAt(r - 1));
+            getText(r - 1, c - 1, str1, str2);
+        } else {
+            if (dp[r - 1][c] > dp[r][c - 1]) {
+                getText(r - 1, c, str1, str2);
+            } else {
+                getText(r, c - 1, str1, str2);
+            }
+        }
     }
 
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         String str1 = br.readLine();
         String str2 = br.readLine();
 
-        String[] answer = T.solution(str1, str2);
-        bw.write(answer[0] + "\n" + answer[1]);
-        bw.flush();
-        bw.close();
+        T.solution(str1, str2);
     }
 }
