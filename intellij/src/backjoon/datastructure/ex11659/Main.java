ff1ace9a29d89dd559d3f50b4bf61e5ea1c86e71
JunYoung-C <doforme524@gmail.com>
2022-06-05 14:27:16 +0900

    success: 자료구조 ex11659 구간 합 구하기4

--- intellij/src/backjoon/datastructure/ex11659/Main.java
+++ intellij/src/backjoon/datastructure/ex11659/Main.java
@@ -3,46 +3,53 @@ package backjoon.datastructure.ex11659;
 import java.io.*;
 import java.util.StringTokenizer;
 
-public class Main {
-    public int solution(int[] sums, int start, int end) {
-        return sums[end] - sums[start - 1];
+class Section {
+    int startIdx;
+    int endIdx;
+
+    public Section(int startIdx, int endIdx) {
+        this.startIdx = startIdx;
+        this.endIdx = endIdx;
     }
+}
 
-    public static void main(String[] args) throws IOException {
-        Main T = new Main();
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+public class Main {
+    public void solution(int n, int[] numbers, int count, Section[] sections) throws IOException {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-
-        st = new StringTokenizer(br.readLine());
-
-        int[] numbers = new int[n + 1];
-        int[] sums = new int[n + 1];
-
+        int[] sumArr = new int[n + 1];
         for (int i = 1; i <= n; i++) {
-            numbers[i] = Integer.parseInt(st.nextToken());
+            sumArr[i] = sumArr[i -  1] + numbers[i];
         }
 
-        calculateSums(n, numbers, sums);
-
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-
-            bw.write(T.solution(sums, start, end)+ "\n");
+        for (Section s : sections) {
+            bw.write(sumArr[s.endIdx] - sumArr[s.startIdx - 1] + "\n");
         }
 
         bw.flush();
         bw.close();
     }
 
-    private static void calculateSums(int n, int[] numbers, int[] sums) {
+    public static void main(String[] args) throws IOException {
+        Main T = new Main();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int count = Integer.parseInt(st.nextToken());
+
+        int[] numbers = new int[n + 1];
+        st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= n; i++) {
-            sums[i] = sums[i - 1] + numbers[i];
+            numbers[i] = Integer.parseInt(st.nextToken());
         }
+
+        Section[] sections = new Section[count];
+        for (int i = 0; i < count; i++) {
+            st = new StringTokenizer(br.readLine());
+            sections[i] = new Section(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+        }
+
+        T.solution(n, numbers, count, sections);
     }
 }
