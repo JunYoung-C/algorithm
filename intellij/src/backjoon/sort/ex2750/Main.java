e1b24c8a74cc0fbe322d63045d4fca27a07f3b95
JunYoung-C <doforme524@gmail.com>
2022-06-07 20:02:28 +0900

    success: 정렬 ex2750 수 정렬하기 버블, 선택, 삽입 정렬로 풀어보았다.

--- intellij/src/backjoon/sort/ex2750/Main.java
+++ intellij/src/backjoon/sort/ex2750/Main.java
@@ -3,41 +3,70 @@ package backjoon.sort.ex2750;
 import java.io.*;
 
 public class Main {
-    public void solution(int n, int[] numbers) throws IOException {
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public int[] solution(int n, int[] numbers) {
+//        bubbleSort(n, numbers);
+//        selectSort(n, numbers);
+        insertSort(n, numbers);
 
-        for (int i = 1; i < n; i++) {
-            for (int j = i; j > 0; j--) {
-                if (numbers[j] < numbers[j - 1]) {
-                    int tmp = numbers[j];
-                    numbers[j] = numbers[j - 1];
-                    numbers[j - 1] = tmp;
-                } else {
-                    break;
+        return numbers;
+    }
+
+    private void bubbleSort(int n, int[] numbers) {
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (numbers[i] > numbers[j]) {
+                    int tmp = numbers[i];
+                    numbers[i] = numbers[j];
+                    numbers[j] = tmp;
                 }
             }
         }
+    }
 
-        for (int number : numbers) {
-            bw.write(number + "\n");
+    private void selectSort(int n, int[] numbers) {
+        for (int i = 0; i < n - 1; i++) {
+            int min = numbers[i];
+            int minIdx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (min > numbers[j]) {
+                    min = numbers[j];
+                    minIdx = j;
+                }
+            }
+
+            int tmp = numbers[i];
+            numbers[i] = numbers[minIdx];
+            numbers[minIdx] = tmp;
         }
+    }
 
-        bw.flush();
-        bw.close();
+    private void insertSort(int n, int[] numbers) {
+        for (int i = 1; i < n; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (numbers[j] > numbers[j + 1]) {
+                    int tmp = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = tmp;
+                }
+            }
+        }
     }
 
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = Integer.parseInt(br.readLine());
-
-        int[] numbers = new int[N];
-        for (int i = 0; i < N; i++) {
+        int n = Integer.parseInt(br.readLine());
+        int[] numbers = new int[n];
+        for (int i = 0; i < n; i++) {
             numbers[i] = Integer.parseInt(br.readLine());
         }
 
-        T.solution(N, numbers);
-
+        for (int i : T.solution(n, numbers)) {
+            bw.write(i + "\n");
+        }
+        bw.flush();
+        bw.close();
     }
 }
