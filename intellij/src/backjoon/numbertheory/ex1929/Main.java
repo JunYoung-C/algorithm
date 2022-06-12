7f2309eb73f191db66aaadf6a498d760c2039971
JunYoung-C <doforme524@gmail.com>
2022-06-12 11:26:25 +0900

    success: 정수론 ex1929 소수 구하기

--- intellij/src/backjoon/numbertheory/ex1929/Main.java
+++ intellij/src/backjoon/numbertheory/ex1929/Main.java
@@ -1,40 +1,48 @@
 package backjoon.numbertheory.ex1929;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    public void solution(int startNum, int endNum) throws IOException {
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        boolean[] isVisited = new boolean[endNum + 1];
-        isVisited[1] = true;
+    public ArrayList<Integer> solution(int startNum, int endNum) {
+        ArrayList<Integer> primes = new ArrayList<>();
+        boolean[] isPrimeNumber = new boolean[endNum + 1];
+        Arrays.fill(isPrimeNumber, true);
+        isPrimeNumber[1] = false;
 
         for (int num = 2; num <= endNum; num++) {
-            if (!isVisited[num]) {
+            if (isPrimeNumber[num]) {
                 for (int i = num * 2; i <= endNum; i += num) {
-                    isVisited[i] = true;
+                    isPrimeNumber[i] = false;
                 }
             }
         }
 
         for (int num = startNum; num <= endNum; num++) {
-            if (!isVisited[num]) {
-                bw.write(num + "\n");
+            if (isPrimeNumber[num]) {
+                primes.add(num);
             }
         }
 
-        bw.flush();
-        bw.close();
+        return primes;
     }
 
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int startNum = Integer.parseInt(st.nextToken());
         int endNum = Integer.parseInt(st.nextToken());
-        T.solution(startNum, endNum);
+
+        br.close();
+        for (int num : T.solution(startNum, endNum)) {
+            bw.write(num+ "\n");
+        }
+        bw.flush();
+        bw.close();
     }
 }
