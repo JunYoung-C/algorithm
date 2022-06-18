927e3b11bab207cce908533bd48df0de714279b2
JunYoung-C <doforme524@gmail.com>
2022-06-18 19:52:04 +0900

    success: 위상 정렬 ex1516 게임 개발

--- intellij/src/backjoon/graph/ex1516/Main.java
+++ intellij/src/backjoon/graph/ex1516/Main.java
@@ -1,76 +1,82 @@
 package backjoon.graph.ex1516;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
+class Building implements Comparable<Building> {
+    int index;
+    int buildTime;
+
+    public Building(int index, int buildTime) {
+        this.index = index;
+        this.buildTime = buildTime;
+    }
+
+    @Override
+    public int compareTo(Building o) {
+        return this.buildTime - o.buildTime;
+    }
+}
 
 public class Main {
-    public int[] solution(int n, int[] degree, int[] buildingTimes, ArrayList<ArrayList<Integer>> graph) {
-        int[] cumulativeTimes = new int[n + 1];
-        Queue<Integer> que = new LinkedList<>();
-        for (int num = 1; num <= n; num++) {
-            cumulativeTimes[num] = buildingTimes[num];
-            if (degree[num] == 0) {
-                que.offer(num);
+    public int[] solution(int buildingType, int[] inDegree, int[] buildTime, ArrayList<ArrayList<Integer>> graph) {
+        int[] answer = new int[buildingType + 1];
+        PriorityQueue<Building> pQ = new PriorityQueue<>();
+
+        for (int i = 1; i <= buildingType; i++) {
+            if (inDegree[i] == 0) {
+                pQ.offer(new Building(i, buildTime[i]));
             }
         }
 
-        while (!que.isEmpty()) {
-            int now = que.poll();
-            // now와 연결된 숫자의 진입 차수 -1하기
-            for (int next : graph.get(now)) {
-                // 진입 차수가 0인 노드 큐에 삽입
-                int sum = cumulativeTimes[now] + buildingTimes[next];
-                if (sum > cumulativeTimes[next]) {
-                    cumulativeTimes[next] = sum;
-                }
-                if (--degree[next] == 0) {
-                    que.offer(next);
+        while (!pQ.isEmpty()) {
+            Building currentBuilding = pQ.poll();
+
+            answer[currentBuilding.index] = currentBuilding.buildTime;
+
+            for (int next : graph.get(currentBuilding.index)) {
+                if (--inDegree[next] == 0) {
+                    pQ.offer(new Building(next, currentBuilding.buildTime + buildTime[next]));
                 }
             }
         }
 
-        return cumulativeTimes;
+
+        return answer;
     }
 
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());
-
-        int[] buildingTimes = new int[n + 1];
-        int[] degree = new int[n + 1];
+        int buildingType = Integer.parseInt(st.nextToken());
         ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-        for (int i = 0; i <= n; i++) {
+        int[] inDegree = new int[buildingType + 1];
+        int[] buildTime = new int[buildingType + 1];
+        for (int i = 0; i <= buildingType; i++) {
             graph.add(new ArrayList<>());
         }
 
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= buildingType; i++) {
             st = new StringTokenizer(br.readLine());
-            buildingTimes[i] = Integer.parseInt(st.nextToken());
-            int num;
-            while (st.hasMoreTokens()) {
-                num = Integer.parseInt(st.nextToken());
-                if (num == -1) {
-                    break;
-                }
-                graph.get(num).add(i);
-                degree[i]++;
+            buildTime[i] = Integer.parseInt(st.nextToken());
+            int prev = Integer.parseInt(st.nextToken());
+
+            while (prev != -1) {
+                graph.get(prev).add(i);
+                inDegree[i]++;
+                prev = Integer.parseInt(st.nextToken());
             }
         }
 
-        int[] times = T.solution(n, degree, buildingTimes, graph);
-        for (int i = 1; i <= n; i++) {
-            bw.write(times[i] + "\n");
-        }
+        br.close();
+        int[] result = T.solution(buildingType, inDegree, buildTime, graph);
 
+        for (int i = 1; i <= buildingType; i++) {
+            bw.write(result[i] + "\n");
+        }
         bw.flush();
         bw.close();
     }
