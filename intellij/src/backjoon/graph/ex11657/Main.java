30f7d8aca84848b873eba073a2cee45e81c3f9a3
JunYoung-C <doforme524@gmail.com>
2022-06-20 22:51:10 +0900

    success: 벨만 포드 ex11657 타임머신

--- intellij/src/backjoon/graph/ex11657/Main.java
+++ intellij/src/backjoon/graph/ex11657/Main.java
@@ -1,70 +1,9 @@
 package backjoon.graph.ex11657;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Objects;
 import java.util.StringTokenizer;
-
-public class Main {
-    public long[] solution(int nodeCount, int edgeCount, Edge[] edges) {
-        long[] minDis = new long[nodeCount + 1];
-        Arrays.fill(minDis, Integer.MAX_VALUE);
-        minDis[1] = 0;
-        for (int i = 1; i < nodeCount; i++) {
-            for (Edge edge : edges) {
-                if (minDis[edge.start] != Integer.MAX_VALUE &&
-                        minDis[edge.end] > minDis[edge.start] + edge.cost) {
-                    minDis[edge.end] = minDis[edge.start] + edge.cost;
-                }
-            }
-        }
-
-        for (Edge edge : edges) {
-            if (minDis[edge.start] != Integer.MAX_VALUE &&
-                    minDis[edge.end] > minDis[edge.start] + edge.cost) {
-                return null;
-            }
-        }
-
-        return minDis;
-    }
-
-    public static void main(String[] args) throws IOException {
-        Main T = new Main();
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int nodeCount = Integer.parseInt(st.nextToken());
-        int edgeCount = Integer.parseInt(st.nextToken());
-
-        Edge[] edges = new Edge[edgeCount];
-
-        for (int i = 0; i < edgeCount; i++) {
-            st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            int cost = Integer.parseInt(st.nextToken());
-            edges[i] = new Edge(start, end, cost);
-        }
-
-        long[] minDis = T.solution(nodeCount, edgeCount, edges);
-        if (Objects.isNull(minDis)) {
-            bw.write("-1\n");
-        } else {
-            for (int i = 2; i <= nodeCount; i++) {
-                if (minDis[i] == Integer.MAX_VALUE) {
-                    bw.write("-1\n");
-                } else {
-                    bw.write(minDis[i] + "\n");
-                }
-            }
-        }
-
-        bw.flush();
-        bw.close();
-    }
-}
 
 class Edge {
     int start;
@@ -75,5 +14,61 @@ class Edge {
         this.start = start;
         this.end = end;
         this.cost = cost;
+    }
+}
+
+public class Main {
+    public void solution(int nodeCount, int edgeCount, ArrayList<Edge> edges) {
+        long[] distance = new long[nodeCount + 1];
+        Arrays.fill(distance, Integer.MAX_VALUE);
+        distance[1] = 0;
+
+        for (int i = 0; i < nodeCount - 1; i++) {
+            for (Edge e : edges) {
+                if (distance[e.start] != Integer.MAX_VALUE && distance[e.end] > distance[e.start] + e.cost) {
+                    distance[e.end] = distance[e.start] + e.cost;
+                }
+            }
+        }
+
+        for (Edge e : edges) {
+            if (distance[e.start] != Integer.MAX_VALUE && distance[e.end] > distance[e.start] + e.cost) {
+                System.out.println(-1);
+                return;
+            }
+        }
+
+        for (int i = 2; i <= nodeCount; i++) {
+            if (distance[i] == Integer.MAX_VALUE) {
+                System.out.println(-1);
+
+            } else {
+                System.out.println(distance[i]);
+
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main T = new Main();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int nodeCount = Integer.parseInt(st.nextToken());
+        int edgeCount = Integer.parseInt(st.nextToken());
+        ArrayList<Edge> edges = new ArrayList<>();
+        for (int i = 0; i < edgeCount; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            edges.add(new Edge(a, b, c));
+        }
+
+
+        br.close();
+        T.solution(nodeCount, edgeCount, edges);
     }
 }
