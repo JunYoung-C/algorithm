f9023fc85d3204b77f89bbb631a29d1d5af7da9f
JunYoung-C <doforme524@gmail.com>
2022-06-24 21:02:24 +0900

    success: 트리 ex11725 트리의 부모 찾기

--- intellij/src/backjoon/tree/ex11725/Main.java
+++ intellij/src/backjoon/tree/ex11725/Main.java
@@ -1,60 +1,56 @@
 package backjoon.tree.ex11725;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    public void solution(int n, ArrayList<ArrayList<Integer>> graph) throws IOException {
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int[] parent;
+    boolean[] isVisited;
+    public void solution(int nodeCount, ArrayList<ArrayList<Integer>> tree) {
+        parent = new int[nodeCount + 1];
+        isVisited = new boolean[nodeCount + 1];
 
-        int[] parent = new int[n + 1];
-        boolean[] isVisited = new boolean[n + 1];
-        Queue<Integer> que = new LinkedList<>();
-        que.offer(1);
-        isVisited[1] = true;
+        dfs(1, tree);
+    }
 
-        while (!que.isEmpty()) {
-            int now = que.poll();
-
-            for (int next : graph.get(now)) {
-                if (!isVisited[next]) {
-                    isVisited[next] = true;
-                    parent[next] = now;
-                    que.offer(next);
-                }
+    private void dfs(int currentNode, ArrayList<ArrayList<Integer>> tree) {
+        for (int next : tree.get(currentNode)) {
+            if (!isVisited[next]) {
+                isVisited[next] = true;
+                parent[next] = currentNode;
+                dfs(next, tree);
             }
         }
-
-        for (int i = 2; i <= n; i++) {
-            bw.write(parent[i]+ "\n");
-        }
-
-        bw.flush();
-        bw.close();
     }
 
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());
+        int nodeCount = Integer.parseInt(st.nextToken());
         ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-        for (int i = 0; i <= n; i++) {
+        for (int i = 0 ; i <= nodeCount; i++) {
             graph.add(new ArrayList<>());
         }
-
-        for (int i = 0; i < n - 1; i++) {
+        for (int i = 0; i < nodeCount - 1; i++) {
             st = new StringTokenizer(br.readLine());
+
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
+
             graph.get(a).add(b);
             graph.get(b).add(a);
         }
 
-        T.solution(n, graph);
+        br.close();
+        T.solution(nodeCount, graph);
+        for (int i = 2; i <= nodeCount; i++) {
+            bw.write(parent[i]+ "\n");
+
+        }
+        bw.flush();
+        bw.close();
     }
 }
