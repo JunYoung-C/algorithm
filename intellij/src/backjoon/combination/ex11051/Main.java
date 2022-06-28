e7592518ec3fa65abbafb252c54959a7b4aafbef
JunYoung-C <doforme524@gmail.com>
2022-06-28 20:27:00 +0900

    success: 조합 ex11051 이항 계수 2

---
+++ intellij/src/backjoon/combination/ex11051/Main.java
@@ -0,0 +1,41 @@
+package backjoon.combination.ex11051;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    int[][] combination = new int[1001][1001];
+
+    public int solution(int n, int k) {
+        return combine(n, k);
+    }
+
+    private int combine(int n, int r) {
+        if (combination[n][r] != 0) {
+            return combination[n][r];
+        }
+
+        if (r == 0 || n == r) {
+            return 1;
+        } else if (r == 1 || n - r == 1) {
+            return n;
+        }
+
+        return combination[n][r] = (combine(n - 1, r - 1) + combine(n - 1, r)) % 10007;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main T = new Main();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        br.close();
+        bw.write(T.solution(n, k)+ "");
+        bw.flush();
+        bw.close();
+    }
+}
