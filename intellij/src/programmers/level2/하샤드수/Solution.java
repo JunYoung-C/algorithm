a52e6b7a223cf2705c48166b08ddef89571eb4c6
JunYoung-C <doforme524@gmail.com>
2022-07-14 09:35:38 +0900

    success: lv1 하샤드 수

---
+++ intellij/src/programmers/level2/하샤드수/Solution.java
@@ -0,0 +1,16 @@
+package programmers.level2.하샤드수;
+
+class Solution {
+    public boolean solution(int x) {
+        int sum = 0;
+        for (char c : String.valueOf(x).toCharArray()) {
+            sum += c - '0';
+        }
+
+        if (x % sum == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
