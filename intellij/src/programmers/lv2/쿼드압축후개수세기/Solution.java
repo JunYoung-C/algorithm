f97de8cfc60763da53916097fbdfcd5a7a5a8ccf
JunYoung-C <doforme524@gmail.com>
2022-08-08 12:40:19 +0900

    success: lv2 쿼드 압축 후 개수 세기

--- intellij/src/programmers/lv2/쿼드압축후개수세기/Solution.java
+++ intellij/src/programmers/lv2/쿼드압축후개수세기/Solution.java
@@ -1,70 +1,38 @@
 package programmers.lv2.쿼드압축후개수세기;
 
-import java.util.*;
-
 class Solution {
-    int[][] points = {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
-
     public int[] solution(int[][] arr) {
-        Area area = dfs(arr, new int[] {0, 0}, arr.length);
-        return new int[] {area.zeroCount, area.oneCount};
+        return dfs(0, 0, arr.length, arr.length, arr);
     }
 
-    // dir : 0 - 좌상, 1 - 우상, 2 - 좌하, 3 - 우하
-    private Area dfs(int[][] arr, int[] point, int length) {
+    private int[] dfs(int sr, int sc, int er, int ec, int[][] arr) { // 0의 개수, 1의 개수
         int zeroCount = 0;
         int oneCount = 0;
-        if (length == 1) {
-            return new Area(arr[0][0] == 0 ? 1 : 0, arr[0][0] == 1 ? 1 : 0);
-        } else if (length == 2) {
-            for (int[] p : points) {
-                if (arr[point[0] + p[0]][point[1] + p[1]] == 0) {
+        int len = er - sr;
+        for (int r = sr; r < er; r++) {
+            for (int c = sc; c < ec; c++) {
+                if (arr[r][c] == 0) {
                     zeroCount++;
                 } else {
                     oneCount++;
                 }
             }
-
-            if (zeroCount == 4) {
-                zeroCount = 1;
-            } else if (oneCount == 4) {
-                oneCount = 1;
-            }
-
-            return new Area(zeroCount, oneCount);
         }
 
-        int[][] startPoints = { // 세로, 가로
-                {point[0], point[1]},
-                {point[0], point[1] + length / 2},
-                {point[0] + length / 2, point[1]},
-                {point[0] + length / 2, point[1] + length / 2}
-        };
+        if (zeroCount == len * len) {
+            return new int[] {1, 0};
+        } else if (oneCount == len * len) {
+            return new int[] {0, 1};
+        } else {
+            int[] result1 = dfs(sr, sc, sr + len / 2, sc + len / 2, arr);
+            int[] result2 = dfs(sr, sc + len / 2, sr + len / 2, ec, arr);
+            int[] result3 = dfs(sr + len / 2, sc, er, sc + len / 2, arr);
+            int[] result4 = dfs(sr + len / 2, sc + len / 2, er, ec, arr);
 
-        zeroCount = 0;
-        oneCount = 0;
-        Area area = null;
-        for (int[] p : startPoints) {
-            area = dfs(arr, p, length / 2);
-            zeroCount += area.zeroCount;
-            oneCount += area.oneCount;
+            zeroCount = result1[0] + result2[0] + result3[0] + result4[0];
+            oneCount = result1[1] + result2[1] + result3[1] + result4[1];
         }
 
-        if (zeroCount == 4 && oneCount == 0) {
-            zeroCount = 1;
-        } else if (oneCount == 4 && zeroCount == 0) {
-            oneCount = 1;
-        }
-        return new Area(zeroCount, oneCount);
-    }
-}
-
-class Area {
-    int zeroCount;
-    int oneCount;
-
-    public Area(int zeroCount, int oneCount) {
-        this.zeroCount = zeroCount;
-        this.oneCount = oneCount;
+        return new int[]{zeroCount, oneCount};
     }
 }
