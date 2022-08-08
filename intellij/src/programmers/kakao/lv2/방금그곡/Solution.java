76d94bb9aa177b75e73321d07e14e53181aedece
JunYoung-C <doforme524@gmail.com>
2022-08-08 11:25:19 +0900

    success: lv2 방금 그 곡

--- intellij/src/programmers/kakao/lv2/방금그곡/Solution.java
+++ intellij/src/programmers/kakao/lv2/방금그곡/Solution.java
@@ -2,106 +2,73 @@ package programmers.kakao.lv2.방금그곡;
 
 import java.util.*;
 
-class Solution {
-    public String solution(String m, String[] musicinfos) {
-        String answer = "";
-        ArrayList<Music> musics = new ArrayList<>();
-        ArrayList<String> mArr = convertToInfoList(m);
-        // musicinfo : "시작 시각,끝난 시각,음악 제목,악보 정보"
-        for (int i = 0; i < musicinfos.length; i++) {
-            String[] infoArr = musicinfos[i].split(",");
-            int playTime = getPlayTime(infoArr[0], infoArr[1]);
-            String musicName = infoArr[2];
-            ArrayList<String> originInfo = convertToInfoList(infoArr[3]);
-            ArrayList<String> musicInfo = convertToInfoList(getMusicInfo(playTime, originInfo));
+import java.util.*;
 
-            if (contains(mArr, musicInfo)) {
-                musics.add(new Music(i, musicName, playTime));
+class Music implements Comparable<Music> {
+    String title;
+    int playTime;
+
+    public Music(String title, int playTime) {
+        this.title = title;
+        this.playTime = playTime;
+    }
+
+    @Override
+    public int compareTo(Music m) {
+        return m.playTime - this.playTime;
+    }
+}
+
+class Solution {
+    Map<String, String> replaceMap =
+            Map.of("C#", "H", "D#", "I", "F#", "J", "G#", "K", "A#", "L");
+
+    public String solution(String m, String[] musicinfos) {
+        ArrayList<Music> musics = new ArrayList<>();
+        m = getReplacedString(m);
+
+        for (String info : musicinfos) { // 시작 시각, 끝난 시각, 음악 제목, 악보 정보
+            String[] splitInfo = info.split(",");
+            int time = getMinuteTime(splitInfo[0], splitInfo[1]);
+            String title = splitInfo[2];
+            String music = getChangedMusic(getReplacedString(splitInfo[3]), time);
+
+            if (music.contains(m)) {
+                musics.add(new Music(title, time));
             }
         }
         if (musics.isEmpty()) {
             return "(None)";
         } else {
             Collections.sort(musics);
-            return musics.get(0).name;
+            return musics.get(0).title;
         }
     }
 
-    private boolean contains(ArrayList<String> info, ArrayList<String> target) {
-        if (info.size() > target.size()) {
-            return false;
-        }
-        boolean contains = false;
-        for (int i = 0; i <= target.size() - info.size(); i++) {
-            for (int j = 0; j < info.size(); j++) {
-                if (!info.get(j).equals(target.get(i + j))) {
-                    break;
-                }
-
-                if (j == info.size() - 1) {
-                    contains = true;
-                }
-            }
-        }
-
-        return contains;
-    }
-
-    private String getMusicInfo(int playTime, ArrayList<String> originInfo) {
+    private String getChangedMusic(String music, int time) {
         StringBuilder sb = new StringBuilder();
-        int len = originInfo.size();
-        for (int i = 0; i < playTime; i++) {
-            sb.append(originInfo.get(i % len));
+        int len = music.length();
+        for (int i = 0; i < time / len; i++) {
+            sb.append(music);
         }
+        sb.append(music.substring(0, time % len));
 
         return sb.toString();
     }
 
-    private ArrayList<String> convertToInfoList(String info) {
-        ArrayList<String> infoList = new ArrayList<>();
-        StringBuilder sb = new StringBuilder();
-        for (char c : info.toCharArray()) {
-            if (sb.length() == 0) {
-                sb.append(c);
-                continue;
-            }
-
-            if (Character.isAlphabetic(c)) {
-                infoList.add(sb.toString());
-                sb.setLength(0);
-            }
-            sb.append(c);
+    private String getReplacedString(String str) {
+        for (String key : replaceMap.keySet()) {
+            str = str.replaceAll(key, replaceMap.get(key));
         }
-        infoList.add(sb.toString());
 
-        return infoList;
+        return str;
     }
 
-    private int getPlayTime(String start, String end) {
-        String[] startArr = start.split(":");
-        String[] endArr = end.split(":");
+    private int getMinuteTime(String start, String end) {
+        String[] splitStart = start.split(":");
+        String[] splitEnd = end.split(":");
 
-        return Integer.parseInt(endArr[0]) * 60 + Integer.parseInt(endArr[1]) -
-                (Integer.parseInt(startArr[0]) * 60 + Integer.parseInt(startArr[1]));
-    }
-}
-
-class Music implements Comparable<Music> {
-    int index;
-    String name;
-    int playTime;
-
-    public Music(int index, String name, int playTime) {
-        this.index = index;
-        this.name = name;
-        this.playTime = playTime;
-    }
-
-    @Override
-    public int compareTo(Music o) {
-        if (o.playTime == this.playTime) {
-            return this.index - o.index;
-        }
-        return o.playTime - this.playTime;
+        return Integer.parseInt(splitEnd[0]) * 60 + Integer.parseInt(splitEnd[1])
+                - (Integer.parseInt(splitStart[0]) * 60 + Integer.parseInt(splitStart[1]));
     }
 }
