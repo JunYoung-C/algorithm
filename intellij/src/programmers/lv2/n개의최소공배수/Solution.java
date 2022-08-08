64043899f0e81449c519a79d6384ec67acc548e7
JunYoung-C <doforme524@gmail.com>
2022-08-08 12:10:31 +0900

    success: lv2 n개의 최소공배수

--- intellij/src/programmers/lv2/n개의최소공배수/Solution.java
+++ intellij/src/programmers/lv2/n개의최소공배수/Solution.java
@@ -5,12 +5,10 @@ import java.util.*;
 class Solution {
     public int solution(int[] arr) {
         int answer = arr[0];
-        int len = arr.length;
-        for (int i = 1; i < len; i++) {
+        for (int i = 1; i < arr.length; i++) {
             int gcd = gcd(answer, arr[i]);
-            answer *= arr[i] / gcd;
+            answer = (answer * arr[i]) / gcd;
         }
-
         return answer;
     }
 
