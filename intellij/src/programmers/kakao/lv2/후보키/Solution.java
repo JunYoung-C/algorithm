776d7a26a401d595f7b4ec27cb30c6ee0097633e
JunYoung-C <doforme524@gmail.com>
2022-08-08 10:53:50 +0900

    success: lv2 후보키

--- intellij/src/programmers/kakao/lv2/후보키/Solution.java
+++ intellij/src/programmers/kakao/lv2/후보키/Solution.java
@@ -3,83 +3,41 @@ package programmers.kakao.lv2.후보키;
 import java.util.*;
 
 class Solution {
-    ArrayList<String> keys = new ArrayList<>();
-
     public int solution(String[][] relation) {
-        int answer = 0;
-        int width = relation[0].length;
-        int height = relation.length;
-        dfs(-1, "", relation, width);
+        int colLen = relation[0].length;
+        int rowLen = relation.length;
+        ArrayList<Integer> conditions = new ArrayList<>();
 
-        Collections.sort(keys, (o1, o2) -> {
-            if (o1.length() == o2.length()) {
-                return o1.compareTo(o2);
+        for (int condition = 1; condition < (1 << colLen); condition++) {
+            HashSet<String> set = new HashSet<>();
+            StringBuilder sb = new StringBuilder();
+            for (int row = 0; row < rowLen; row++) {
+                for (int col = 0; col < colLen; col++) {
+                    if ((condition & (1 << col)) > 0) {
+                        sb.append(relation[row][col]);
+                        sb.append(" ");
+                    }
+                }
+
+                set.add(sb.toString());
+                sb.setLength(0);
             }
 
-            return o1.length() - o2.length();
-        });
-
-        ArrayList<String> uniqueKeys = new ArrayList<>();
-        for (String key : keys) {
-            if (satisfyMinimality(key, uniqueKeys)) {
-                uniqueKeys.add(key);
+            if (set.size() == rowLen && isPossible(condition, conditions)) {
+                conditions.add(condition);
             }
         }
 
-        return uniqueKeys.size();
+        return conditions.size();
     }
 
-    private void dfs(int lastIndex, String key, String[][] relation, int len) {
-
-        if (!key.equals("") && satisfyUniqueness(relation, key)) {
-            keys.add(key);
-        }
-
-        if (key.length() == len) {
-            return;
-        }
-        for (int i = lastIndex + 1; i < len; i++) {
-            dfs(i, key + i, relation, len);
-        }
-    }
-
-    private boolean satisfyUniqueness(String[][] relation, String key) {
-        int rowCount = relation.length;
-        HashSet<String> set = new HashSet<>();
-        StringBuilder sb;
-        for (String[] tuple : relation) {
-            sb = new StringBuilder();
-            for (char c : key.toCharArray()) {
-                sb.append(tuple[c - '0']);
-            }
-            set.add(sb.toString());
-        }
-
-        if (set.size() != rowCount) {
-            return false;
-        }
-        return true;
-    }
-
-    private boolean satisfyMinimality(String target, ArrayList<String> keys) {
-        HashMap<Character, Boolean> map = new HashMap<>();
-
-        for (String key : keys) {
-            map.clear();
-
-            for (char c : target.toCharArray()) {
-                map.put(c, true);
-            }
-
-            for (char c : key.toCharArray()) {
-                map.put(c, true);
-            }
-
-            if (map.size() == target.length()) {
+    private boolean isPossible(int condition, ArrayList<Integer> conditions) {
+        for (int c : conditions) {
+            if ((c & condition) == c) {
                 return false;
             }
         }
-
         return true;
     }
+
 }
