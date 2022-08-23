fcc994cd04304a596169dd83eb35f964ad2eeb45
JunYoung-C <doforme524@gmail.com>
2022-08-23 14:49:47 +0900

    success: lv3 스타 수열

--- intellij/src/programmers/lv3/스타수열/Solution.java
+++ intellij/src/programmers/lv3/스타수열/Solution.java
@@ -3,52 +3,59 @@ package programmers.lv3.스타수열;
 import java.util.*;
 
 class Solution {
-    ArrayList<Integer> numbers = new ArrayList<>();
     public int solution(int[] a) {
         int answer = -1;
-        int len = a.length;
-        if (len == 1) {
+        if (a.length == 1) {
             return 0;
-        } else if (len == 2) {
+        } else if (a.length == 2) {
             if (a[0] == a[1]) {
-                return 2;
+                return 0;
             } else {
                 return 1;
             }
         }
-
-        if (a[0] != a[1]) {
-            numbers.add(a[0]);
-        }
-
-        for (int i = 1; i < len - 1; i++) {
-            if (a[i] == a[i - 1] && a[i] == a[i + 1]) {
-                continue;
-            }
-
-            numbers.add(a[i]);
-        }
-
-        if (a[len - 2] != a[len - 1]) {
-            numbers.add(a[len - 1]);
-        }
-
         HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i : numbers) {
+        for (int i : a) {
             map.put(i, map.getOrDefault(i, 0) + 1);
         }
 
-        int max = 0;
-        for (int key : map.keySet()) {
-            if (map.get(key) > max) {
-                max = map.get(key);
+        ArrayList<Integer> numbers = new ArrayList<>(map.keySet());
+        Collections.sort(numbers, (o1, o2) -> {
+            return map.get(o2) - map.get(o1);
+        });
+
+        for (int num : numbers) {
+            if (map.get(num) <= 1 ) {
+                break;
             }
+
+            boolean keepNum = false;
+            boolean keepOtherNum = false;
+
+            int count = 0;
+            for (int i : a) {
+                if (i == num) {
+                    if (keepOtherNum) {
+                        count += 2;
+                        keepNum = false;
+                        keepOtherNum = false;
+                    } else {
+                        keepNum = true;
+                    }
+                } else {
+                    if (keepNum) {
+                        count += 2;
+                        keepNum = false;
+                        keepOtherNum = false;
+                    } else {
+                        keepOtherNum = true;
+                    }
+                }
+            }
+
+            answer = Math.max(answer, count);
         }
 
-        answer = max / 2 * 4;
-        if (answer > len) {
-            answer -= 2;
-        }
         return answer;
     }
 }
