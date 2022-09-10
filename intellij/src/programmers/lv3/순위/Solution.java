83a1e9df90f8212c32573e649df07ef17a549528
JunYoung-C <doforme524@gmail.com>
2022-09-10 10:49:34 +0900

    success: lv3  순위

--- intellij/src/programmers/lv3/순위/Solution.java
+++ intellij/src/programmers/lv3/순위/Solution.java
@@ -1,53 +1,38 @@
 package programmers.lv3.순위;
 
-import java.util.*;
-
 class Solution {
     public int solution(int n, int[][] results) {
         int answer = 0;
-
-        int[][] graph = new int[n + 1][n + 1];
+        // 1 : a가 b를 이김, -1 : a가 b에게 짐, 0 : 알 수 없음
+        int[][] game = new int[n + 1][n + 1];
         for (int[] result : results) {
-            graph[result[0]][result[1]] = 1;
-            graph[result[1]][result[0]] = -1;
+            game[result[0]][result[1]] = 1;
+            game[result[1]][result[0]] = -1;
         }
 
         for (int k = 1; k <= n; k++) {
-            for (int r = 1; r <= n; r++) {
-                for (int c = 1; c <= n; c++) {
-                    if (r == c) {
+            for (int a = 1; a <= n; a++) {
+                for (int b = 1; b <= n; b++) {
+                    if (a == b) {
                         continue;
                     }
-
-                    if (graph[r][k] == 1 && graph[k][c] == 1) {
-                        graph[r][c] = 1;
-                    } else if (graph[r][k] == -1 && graph[k][c] == -1) {
-                        graph[r][c] = -1;
+                    if (game[a][k] != 0 && game[a][k] == game[k][b]) {
+                        game[a][b] = game[a][k];
                     }
                 }
             }
         }
 
-        for (int r = 1; r <= n; r++) {
-            int winCount = 0;
-            int loseCount = 0;
-            for (int c = 1; c <= n; c++) {
-                if (graph[r][c] == 1) {
-                    winCount++;
-                } else if (graph[r][c] == -1) {
-                    loseCount++;
+        for (int a = 1; a <= n; a++) {
+            int count = 0;
+            for (int b = 1; b <= n; b++) {
+                if (game[a][b] != 0) {
+                    count++;
                 }
             }
-            if (winCount + loseCount == n - 1) {
+            if (count == n - 1) {
                 answer++;
             }
-        }
-
-        for (int r = 1; r <= n; r++) {
-            for (int c = 1; c <= n; c++) {
-                System.out.print(graph[r][c] + "\t");
-            }
-            System.out.println();
         }
 
         return answer;
