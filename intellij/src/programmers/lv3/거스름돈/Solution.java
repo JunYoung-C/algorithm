4dd35c788869165d954ed9f10bba90482da540a9
JunYoung-C <doforme524@gmail.com>
2022-09-11 12:29:01 +0900

    success: lv3 거스름돈

--- intellij/src/programmers/lv3/거스름돈/Solution.java
+++ intellij/src/programmers/lv3/거스름돈/Solution.java
@@ -1,24 +1,23 @@
 package programmers.lv3.거스름돈;
 
+import java.util.*;
+
 class Solution {
     public int solution(int n, int[] money) {
         int answer = 0;
-        int moneyLen = money.length;
-        int[][] dp = new int[moneyLen + 1][n + 1];
-        for (int i = 1; i <= moneyLen; i++) {
-            dp[i][0] = 1;
-        }
+        int[] dp = new int[n + 1];
+        Arrays.sort(money);
 
-        for (int row = 1; row <= moneyLen; row++) {
-            for (int j = 1; j <= n; j++) {
-                if (j - money[row - 1] < 0) {
-                    dp[row][j] = dp[row - 1][j];
-                } else {
-                    dp[row][j] = (dp[row][j - money[row - 1]] + dp[row - 1][j]) % 1000000007;
-                }
+        for (int m : money) {
+            if (m <= n) {
+                dp[m]++;
+            }
+
+            for (int i = m + 1; i <= n; i++) {
+                dp[i] = (dp[i - m] + dp[i]) % 1_000_000_007;
             }
         }
 
-        return dp[moneyLen][n];
+        return dp[n];
     }
 }
