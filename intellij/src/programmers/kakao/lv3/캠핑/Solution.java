169ee912cb76b7751743fd95142b09c8f316337e
JunYoung-C <doforme524@gmail.com>
2022-09-12 13:11:39 +0900

    wrong: lv3 캠핑 구간합을 잊지말자

--- intellij/src/programmers/kakao/lv3/캠핑/Solution.java
+++ intellij/src/programmers/kakao/lv3/캠핑/Solution.java
@@ -5,9 +5,7 @@ import java.util.*;
 class Solution {
     public int solution(int n, int[][] data) {
         int answer = 0;
-        int[][] dp = new int[5000][5000];
 
-        // 좌표 압축
         ArrayList<Integer> xList = new ArrayList<>();
         ArrayList<Integer> yList = new ArrayList<>();
 
@@ -16,21 +14,28 @@ class Solution {
             yList.add(p[1]);
         }
 
-        ArrayList<Integer> uniqueXList = new ArrayList<>(new HashSet<Integer>(xList));
-        ArrayList<Integer> uniqueYList = new ArrayList<>(new HashSet<Integer>(yList));
+        ArrayList<Integer> uniqueXList = new ArrayList<>(new HashSet<>(xList));
+        ArrayList<Integer> uniqueYList = new ArrayList<>(new HashSet<>(yList));
 
         Collections.sort(uniqueXList);
         Collections.sort(uniqueYList);
 
-        for (int i = 0; i < n; i++) {
-            data[i][0] = uniqueXList.indexOf(xList.get(i));
-            data[i][1] = uniqueYList.indexOf(yList.get(i));
+        int[][] dp = new int[5000][5000];
 
-            dp[data[i][1]][data[i][0]] = 1;
+        for (int[] p : data) {
+            p[0] = uniqueXList.indexOf(p[0]);
+            p[1] = uniqueYList.indexOf(p[1]);
+            dp[p[1]][p[0]]++;
         }
 
 
-        // dp(구간합) 구하기
+        Arrays.sort(data, (p1, p2) -> {
+            if (p1[0] == p2[0]) {
+                return p1[1] - p2[1];
+            }
+            return p1[0] - p2[0];
+        });
+
         for (int r = 0; r < 5000; r++) {
             for (int c = 0; c < 5000; c++) {
                 if (r - 1 >= 0) {
@@ -41,37 +46,38 @@ class Solution {
                     dp[r][c] += dp[r][c - 1];
                 }
 
-                if (r - 1 >= 0 & c - 1 >= 0) {
+                if (r - 1 >= 0 && c - 1 >= 0) {
                     dp[r][c] -= dp[r - 1][c - 1];
                 }
             }
         }
 
-        Arrays.sort(data, (o1, o2) -> {
-            if (o1[0] == o2[0]) {
-                return o1[1] - o2[1];
-            }
-            return o1[0] - o2[0];
-        });
-
         for (int i = 0; i < n; i++) {
+            int x1 = data[i][0];
+            int y1 = data[i][1];
             for (int j = i + 1; j < n; j++) {
-                int x1 = Math.min(data[i][0], data[j][0]);
-                int y1 = Math.min(data[i][1], data[j][1]);
-                int x2 = Math.max(data[i][0], data[j][0]);
-                int y2 = Math.max(data[i][1], data[j][1]);
-
-                if (x1 == x2 || y1 == y2) {
-                    continue;
-                }
-
-                int count = dp[y2 - 1][x2 - 1] - dp[y2 - 1][x1] - dp[y1][x2 - 1] + dp[y1][x1];
-                if (count == 0) {
+                int x2 = data[j][0];
+                int y2 = data[j][1];
+                if (isValid(Math.min(x1, x2), Math.min(y1, y2),
+                        Math.max(x1, x2), Math.max(y1, y2), dp)) {
                     answer++;
                 }
             }
         }
 
         return answer;
+    }
+
+    private boolean isValid(int x1, int y1, int x2, int y2, int[][] dp) {
+        if (x1 == x2 || y1 == y2) {
+            return false;
+        }
+
+        int count = dp[y2 - 1][x2 - 1] - dp[y2 - 1][x1] - dp[y1][x2 - 1] + dp[y1][x1];
+        if (count == 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
