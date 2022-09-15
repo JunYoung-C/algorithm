7171ce5e6ff411cf0307641f4af2624e9d8d39d8
JunYoung-C <doforme524@gmail.com>
2022-09-15 11:15:21 +0900

    *success: lv3 길찾기 게임 다음에는 트리를 구현해서 풀어보자

--- intellij/src/programmers/kakao/lv3/길찾기게임/Solution.java
+++ intellij/src/programmers/kakao/lv3/길찾기게임/Solution.java
@@ -3,82 +3,59 @@ package programmers.kakao.lv3.길찾기게임;
 import java.util.*;
 
 class Solution {
-    int height = 0, width = 0;
-    int[][] nodeList;
-    ArrayList<Integer> prefixList = new ArrayList<>();
-    ArrayList<Integer> postfixList = new ArrayList<>();
+    ArrayList<Integer> heights;
+    ArrayList<Integer> preorder = new ArrayList<>();
+    ArrayList<Integer> postorder = new ArrayList<>();
 
-    public int[][] solution(int[][] nodeInfo) {
+    public int[][] solution(int[][] nodeinfo) {
         int[][] answer = {};
-        if (nodeInfo.length == 1) {
-            return new int[][] {{1}, {1}};
-        }
+        initHeights(nodeinfo);
 
-        compress(nodeInfo);
+        setOrder(0, 0, 100000, nodeinfo);
 
-        nodeList = new int[height][width];
-        int index = 1;
-        for (int[] node : nodeInfo) {
-            nodeList[node[1]][node[0]] = index++;
-        }
-
-        dfs(0, width - 1, height - 1);
-
-        answer = new int[2][width];
-        for (int i = 0; i < width; i++) {
-            answer[0][i] = prefixList.get(i);
-            answer[1][i] = postfixList.get(i);
+        int len = nodeinfo.length;
+        answer = new int[2][len];
+        for (int i = 0; i < len; i++) {
+            answer[0][i] = preorder.get(i);
+            answer[1][i] = postorder.get(i);
         }
 
         return answer;
     }
 
-    private void dfs(int x1, int x2, int y) {
-        if (y == -1) {
+    private void setOrder(int heightIndex, int minX, int maxX, int[][] nodeInfo) {
+        if (heightIndex >= heights.size()) {
             return;
         }
+        int parentHeight = heights.get(heightIndex);
+        int parentX = -1;
+        int parentIndex = -1;
 
-        int x = x1;
-        for (; x <= x2; x++) {
-            if (nodeList[y][x] != 0) {
-                prefixList.add(nodeList[y][x]);
-                break;
+        for (int i = 0; i < nodeInfo.length; i++) {
+            int[] node = nodeInfo[i];
+            if (node[1] == parentHeight && node[0] >= minX && node[0] <= maxX) {
+                parentIndex = i;
+                parentX = node[0];
             }
         }
 
-        if (x - 1 >= x1) {
-            dfs(x1, x - 1, y - 1);
+        if (parentIndex == -1) {
+            return;
         }
 
-        if (x + 1 <= x2) {
-            dfs(x + 1, x2, y - 1);
-        }
-
-        postfixList.add(nodeList[y][x]);
+        preorder.add(parentIndex + 1);
+        setOrder(heightIndex + 1, minX, parentX - 1, nodeInfo);
+        setOrder(heightIndex + 1, parentX + 1, maxX, nodeInfo);
+        postorder.add(parentIndex + 1);
     }
 
-    private void compress(int[][] nodeInfo) {
-        ArrayList<Integer> xList = new ArrayList<>();
-        ArrayList<Integer> yList = new ArrayList<>();
-
-        for (int[] node : nodeInfo) {
-            xList.add(node[0]);
-            yList.add(node[1]);
+    private void initHeights(int[][] nodeinfo) {
+        HashSet<Integer> heightSet = new HashSet<>();
+        for (int[] node : nodeinfo) {
+            heightSet.add(node[1]);
         }
-
-        ArrayList<Integer> uniqueXList = new ArrayList<>(new HashSet<>(xList));
-        ArrayList<Integer> uniqueYList = new ArrayList<>(new HashSet<>(yList));
-
-        Collections.sort(uniqueXList);
-        Collections.sort(uniqueYList);
-
-        height = uniqueYList.size();
-        width = uniqueXList.size();
-
-        for (int[] node : nodeInfo) {
-            node[0] = uniqueXList.indexOf(node[0]);
-            node[1] = uniqueYList.indexOf(node[1]);
-        }
+        heights = new ArrayList<>(heightSet);
+        Collections.sort(heights, Collections.reverseOrder());
     }
 }
 
