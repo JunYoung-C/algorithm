b1c73eb1aa0b123857256d57ace1569d639348e5
JunYoung-C <doforme524@gmail.com>
2022-09-16 14:30:39 +0900

    success: lv3 자물쇠와 열쇠

--- intellij/src/programmers/kakao/lv3/자물쇠와열쇠/Solution.java
+++ intellij/src/programmers/kakao/lv3/자물쇠와열쇠/Solution.java
@@ -2,22 +2,30 @@ package programmers.kakao.lv3.자물쇠와열쇠;
 
 class Solution {
     public boolean solution(int[][] key, int[][] lock) {
-        lock = createLargeLock(lock, key.length);
+        lock = getBigLock(key.length, lock);
+
         for (int i = 0; i < 4; i++) {
-            if (isAbleToCombine(key, lock)) {
+            if (isPossible(key, lock)) {
                 return true;
             }
-            key = rotate(key);
+            System.out.println();
+            rotate(key);
         }
 
-
+        // for (int r = 0; r < lock.length; r++) {
+        //     for (int c = 0; c < lock.length; c++) {
+        //         System.out.print(lock[r][c] + " ");
+        //     }
+        //     System.out.println();
+        // }
         return false;
     }
 
-    private boolean isAbleToCombine(int[][] key, int[][] lock) {
-        for (int r = 0; r <= lock.length - key.length; r++) {
-            for (int c = 0; c <= lock.length - key.length; c++) {
-                if (isCombined(r, c, lock, key)) {
+    private boolean isPossible(int[][] key, int[][] lock) {
+        int lockLen = lock.length, keyLen = key.length;
+        for (int r = 0; r <= lockLen - keyLen; r++) {
+            for (int c = 0; c <= lockLen - keyLen; c++) {
+                if (isSame(key, lock, r, c)) {
                     return true;
                 }
             }
@@ -26,62 +34,63 @@ class Solution {
         return false;
     }
 
-    private boolean isCombined(int r1, int c1, int[][] lock, int[][] key) {
-        int keyLen = key.length;
-        int len = lock.length;
-        boolean isCombined = true;
-        for (int r = r1; r < r1 + keyLen; r++) {
-            for (int c = c1; c < c1 + keyLen; c++) {
-                lock[r][c] += key[r - r1][c - c1];
+    private boolean isSame(int[][] key, int[][] lock, int sr, int sc) {
+        boolean isSame = true;
+        int lockLen = lock.length, keyLen = key.length;
+        for (int r = 0; r < keyLen; r++) {
+            for (int c = 0; c < keyLen; c++) {
+                lock[r + sr][c + sc] += key[r][c];
             }
         }
 
-        for (int r = keyLen - 1; r <= len - keyLen; r++) {
-            for (int c = keyLen - 1; c <= len - keyLen; c++) {
-                // System.out.println(r + " " + c);
+        for (int r = keyLen - 1; r <= lockLen - keyLen; r++) {
+            for (int c = keyLen - 1; c <= lockLen - keyLen; c++) {
                 if (lock[r][c] != 1) {
-                    isCombined = false;
+                    isSame = false;
                 }
+                // System.out.print(lock[r][c] + " ");
             }
+            // System.out.println();
         }
-
-        // for (int r = 0; r < len; r++) {
-        //     for (int c = 0; c < len; c++) {
-        //         System.out.print(lock[r][c] + " ");
-        //     }
-        //     System.out.println();
-        // }
         // System.out.println();
 
-        for (int r = r1; r < r1 + keyLen; r++) {
-            for (int c = c1; c < c1 + keyLen; c++) {
-                lock[r][c] -= key[r - r1][c - c1];
+        for (int r = 0; r < keyLen; r++) {
+            for (int c = 0; c < keyLen; c++) {
+                lock[r + sr][c + sc] -= key[r][c];
             }
         }
-        return isCombined;
+
+        return isSame;
     }
 
-    private int[][] createLargeLock(int[][] lock, int keyLength) {
-        int len = keyLength * 2 + lock.length - 2;
-        int[][] largeLock = new int[len][len];
-        for (int r = 0; r < lock.length; r++) {
-            for (int c = 0; c < lock.length; c++) {
-                largeLock[r + keyLength - 1][c + keyLength - 1] = lock[r][c];
+    private int[][] getBigLock(int keyLen, int[][] lock) {
+        int lockLen = lock.length;
+        int len = lockLen + 2 * keyLen - 2;
+        int[][] bigLock = new int[len][len];
+
+        for (int r = 0; r < lockLen; r++) {
+            for (int c = 0; c < lockLen; c++) {
+                bigLock[r + keyLen - 1][c + keyLen - 1] = lock[r][c];
             }
         }
-        return largeLock;
+
+        return bigLock;
     }
 
-    private int[][] rotate(int[][] board) {
+    private void rotate(int[][] board) {
         int len = board.length;
-        int[][] rotatedBoard = new int[len][len];
+        int[][] tmp = new int[len][len];
 
-        for (int row = 0; row < len; row++) {
-            for (int col = 0; col < len; col++) {
-                rotatedBoard[col][len - 1 - row] = board[row][col];
+        for (int r = 0; r < len; r++) {
+            for (int c = 0; c < len; c++) {
+                tmp[c][len - 1 - r] = board[r][c];
             }
         }
 
-        return rotatedBoard;
+        for (int r = 0; r < len; r++) {
+            for (int c = 0; c < len; c++) {
+                board[r][c] = tmp[r][c];
+            }
+        }
     }
 }
