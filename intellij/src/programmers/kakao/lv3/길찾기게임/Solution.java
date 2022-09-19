ef5463a0c831a96a5427d1408537f1cf1bf64ccf
JunYoung-C <doforme524@gmail.com>
2022-09-20 05:33:34 +0900

    success: lv3 길 찾기 게임 이진 트리를 생성하는 방법을 사용하였다.

--- intellij/src/programmers/kakao/lv3/길찾기게임/Solution.java
+++ intellij/src/programmers/kakao/lv3/길찾기게임/Solution.java
@@ -3,17 +3,29 @@ package programmers.kakao.lv3.길찾기게임;
 import java.util.*;
 
 class Solution {
-    ArrayList<Integer> heights;
-    ArrayList<Integer> preorder = new ArrayList<>();
-    ArrayList<Integer> postorder = new ArrayList<>();
-
     public int[][] solution(int[][] nodeinfo) {
         int[][] answer = {};
-        initHeights(nodeinfo);
-
-        setOrder(0, 0, 100000, nodeinfo);
-
         int len = nodeinfo.length;
+        int[][] nodeInfo = new int[len][3]; // 인덱스, x, y
+        initNodeInfo(nodeInfo, nodeinfo);
+
+        Tree tree = null;
+        for (int[] n : nodeInfo) {
+            Node node = new Node(n[0], n[1], n[2]);
+            if (tree == null) {
+                tree = new Tree(node);
+                continue;
+            }
+
+            tree.add(node);
+        }
+
+        ArrayList<Integer> preorder = new ArrayList<>();
+        tree.setPreorder(preorder, tree.root);
+
+        ArrayList<Integer> postorder = new ArrayList<>();
+        tree.setPostorder(postorder, tree.root);
+
         answer = new int[2][len];
         for (int i = 0; i < len; i++) {
             answer[0][i] = preorder.get(i);
@@ -23,39 +35,81 @@ class Solution {
         return answer;
     }
 
-    private void setOrder(int heightIndex, int minX, int maxX, int[][] nodeInfo) {
-        if (heightIndex >= heights.size()) {
-            return;
+    private void initNodeInfo(int[][] nodeInfo, int[][] nodeinfo) {
+        int len = nodeinfo.length;
+        for (int i = 0; i < len; i++) {
+            nodeInfo[i][0] = i + 1;
+            nodeInfo[i][1] = nodeinfo[i][0];
+            nodeInfo[i][2] = nodeinfo[i][1];
         }
-        int parentHeight = heights.get(heightIndex);
-        int parentX = -1;
-        int parentIndex = -1;
 
-        for (int i = 0; i < nodeInfo.length; i++) {
-            int[] node = nodeInfo[i];
-            if (node[1] == parentHeight && node[0] >= minX && node[0] <= maxX) {
-                parentIndex = i;
-                parentX = node[0];
+        Arrays.sort(nodeInfo, (o1, o2) -> {
+            if (o1[2] == o2[2]) {
+                return o1[1] - o2[1];
             }
-        }
-
-        if (parentIndex == -1) {
-            return;
-        }
-
-        preorder.add(parentIndex + 1);
-        setOrder(heightIndex + 1, minX, parentX - 1, nodeInfo);
-        setOrder(heightIndex + 1, parentX + 1, maxX, nodeInfo);
-        postorder.add(parentIndex + 1);
-    }
-
-    private void initHeights(int[][] nodeinfo) {
-        HashSet<Integer> heightSet = new HashSet<>();
-        for (int[] node : nodeinfo) {
-            heightSet.add(node[1]);
-        }
-        heights = new ArrayList<>(heightSet);
-        Collections.sort(heights, Collections.reverseOrder());
+            return o2[2] - o1[2];
+        });
     }
 }
 
+class Tree {
+    Node root;
+
+    public Tree(Node root) {
+        this.root = root;
+    }
+
+    public void add(Node node) {
+        Node cNode = root;
+
+        while (true) {
+            if (cNode.x > node.x) {
+                if (cNode.left == null) {
+                    cNode.left = node;
+                    return;
+                } else {
+                    cNode = cNode.left;
+                }
+            } else {
+                if (cNode.right == null) {
+                    cNode.right = node;
+                    return;
+                } else {
+                    cNode = cNode.right;
+                }
+            }
+        }
+    }
+
+    public void setPreorder(ArrayList<Integer> preorder, Node cNode) {
+        if (cNode == null) {
+            return;
+        }
+
+        preorder.add(cNode.index);
+        setPreorder(preorder, cNode.left);
+        setPreorder(preorder, cNode.right);
+    }
+
+    public void setPostorder(ArrayList<Integer> postorder, Node cNode) {
+        if (cNode == null) {
+            return;
+        }
+
+        setPostorder(postorder, cNode.left);
+        setPostorder(postorder, cNode.right);
+        postorder.add(cNode.index);
+    }
+}
+
+class Node {
+    int index, x, y;
+    Node left;
+    Node right;
+
+    public Node(int index, int x, int y) {
+        this.index = index;
+        this.x = x;
+        this.y = y;
+    }
+}
