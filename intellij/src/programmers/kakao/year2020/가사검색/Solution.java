7c6769f03d1009d7458ba337c2882e800cdfcb8f
JunYoung-C <doforme524@gmail.com>
2022-09-23 16:31:16 +0900

    success: lv4 가사 검색

--- intellij/src/programmers/kakao/year2020/가사검색/Solution.java
+++ intellij/src/programmers/kakao/year2020/가사검색/Solution.java
@@ -1,101 +1,91 @@
 package programmers.kakao.year2020.가사검색;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
+
+class Trie {
+    Trie[] child = new Trie[26];
+    int count;
+
+    public Trie() {
+    }
+
+    public void add(String str) {
+        Trie current = this;
+        int index = 0;
+        current.count++;
+
+        while (index < str.length()) {
+            char c = str.charAt(index++);
+
+            if (current.child[c - 'a'] == null) {
+                current.child[c - 'a'] = new Trie();
+            }
+
+            current = current.child[c - 'a'];
+            current.count++;
+        }
+    }
+
+    public int getCount(String str) {
+        Trie current = this;
+        int index = 0;
+
+        while (index < str.length()) {
+            char c = str.charAt(index++);
+
+            if (c == '?') {
+                return current.count;
+            } else {
+                current = current.child[c - 'a'];
+            }
+
+            if (current == null) {
+                return 0;
+            }
+        }
+
+        return current.count;
+    }
+}
 
 class Solution {
     public int[] solution(String[] words, String[] queries) {
         int len = queries.length;
         int[] answer = new int[len];
-        HashMap<Integer, ArrayList<String>> reversedWordsMap = new HashMap<>(), wordsMap = new HashMap<>();
-        initMap(words, reversedWordsMap, wordsMap);
+
+        HashMap<Integer, Trie> wordTrieMap = new HashMap<>();
+        HashMap<Integer, Trie> reversedWordTrieMap = new HashMap<>();
+
+        for (String word : words) {
+            addWordEntry(wordTrieMap, word);
+            addWordEntry(reversedWordTrieMap, reverse(word));
+        }
 
         for (int i = 0; i < len; i++) {
             String query = queries[i];
-            ArrayList<String> wordList;
 
             if (query.charAt(0) == '?') {
-                if (!reversedWordsMap.containsKey(query.length())) {
-                    continue;
-                } else {
-                    wordList = reversedWordsMap.get(query.length());
-                    query = reverse(query);
+                if (reversedWordTrieMap.containsKey(query.length())) {
+                    answer[i] = reversedWordTrieMap.get(query.length()).getCount(reverse(query));
                 }
             } else {
-                if (!wordsMap.containsKey(query.length())) {
-                    continue;
-                } else {
-                    wordList = wordsMap.get(query.length());
+                if (wordTrieMap.containsKey(query.length())) {
+                    answer[i] = wordTrieMap.get(query.length()).getCount(query);
                 }
             }
-
-            answer[i] = getUpperIndex(wordList, query.replace('?', 'z'))
-                    - getLowerIndex(wordList, query.replace('?', 'a')) + 1;
         }
 
         return answer;
     }
 
-    private int getUpperIndex(ArrayList<String> words, String target) {
-        int lt = 0, rt = words.size() - 1;
-        int index = -1;
+    private static void addWordEntry(HashMap<Integer, Trie> wordTrieMap, String word) {
+        int wordLen = word.length();
 
-        while (lt <= rt) {
-            int mid = (lt + rt) / 2;
-
-            if (target.compareTo(words.get(mid)) < 0) {
-                rt = mid - 1;
-            } else {
-                lt = mid + 1;
-                index = mid;
-            }
+        if (!wordTrieMap.containsKey(wordLen)) {
+            wordTrieMap.put(wordLen, new Trie());
         }
 
-        return index;
-    }
-
-    private int getLowerIndex(ArrayList<String> words, String target) {
-        int lt = 0, rt = words.size() - 1;
-        int index = words.size();
-
-        while (lt <= rt) {
-            int mid = (lt + rt) / 2;
-
-            if (target.compareTo(words.get(mid)) <= 0) {
-                rt = mid - 1;
-                index = mid;
-            } else {
-                lt = mid + 1;
-            }
-        }
-
-        return index;
-    }
-
-    private void sortMap(HashMap<Integer, ArrayList<String>> wordsMap) {
-        for (ArrayList<String> words : wordsMap.values()) {
-            Collections.sort(words);
-        }
-    }
-
-    private void initMap(String[] words, HashMap<Integer, ArrayList<String>> reversedWordsMap, HashMap<Integer, ArrayList<String>> wordsMap) {
-        for (String word : words) {
-            int wordLen = word.length();
-            if (!wordsMap.containsKey(wordLen)) {
-                wordsMap.put(wordLen, new ArrayList<>());
-            }
-
-            if (!reversedWordsMap.containsKey(wordLen)) {
-                reversedWordsMap.put(wordLen, new ArrayList<>());
-            }
-
-            wordsMap.get(wordLen).add(word);
-            reversedWordsMap.get(wordLen).add(reverse(word));
-        }
-
-        sortMap(wordsMap);
-        sortMap(reversedWordsMap);
+        wordTrieMap.get(wordLen).add(word);
     }
 
     private String reverse(String str) {
