d5a15c0fab6d6da8a51a3ba7503559ccecc17f97
JunYoung-C <doforme524@gmail.com>
2022-10-20 10:40:32 +0900

    success: ex11053 가장 긴 증가하는 부분수열

---
+++ intellij/src/backjoon/dp/ex11053/Main.java
@@ -0,0 +1,40 @@
+package backjoon.dp.ex11053;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public int solution(int n, int[] numbers) {
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        for (int i = 1; i < n; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (numbers[i] > numbers[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        return Arrays.stream(dp).max().getAsInt();
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main T = new Main();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int[] numbers = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+
+        br.close();
+        bw.write(T.solution(n, numbers)+ "");
+        bw.flush();
+        bw.close();
+    }
+}
