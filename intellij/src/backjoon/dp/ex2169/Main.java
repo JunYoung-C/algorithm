b852e99f85eeac43d5358b1fd96205f0cd9c52b4
JunYoung-C <doforme524@gmail.com>
2022-11-04 12:07:11 +0900

    success: ex2169 로봇 조종하기

--- intellij/src/backjoon/dp/ex2169/Main.java
+++ intellij/src/backjoon/dp/ex2169/Main.java
@@ -5,51 +5,45 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    int[] dx = {1, 0, -1};
-    int[] dy = {0, 1, 0};
-    int[][] dp;
-    boolean[][] isVisited;
-
     public int solution(int width, int height, int[][] board) {
-        dp  = new int[height][width];
-        isVisited = new boolean[height][width];
-        isVisited[0][0] = true;
-        for (int i = 0; i < height; i++) {
-            Arrays.fill(dp[i], -100_001);
-        }
+        final int RIGHT = 0;
+        final int LEFT = 1;
+        int[][] dp  = new int[height][width];
+        int[][] rowDp = new int[2][width]; // 우, 좌
 
         dp[0][0] = board[0][0];
-        dfs(width, height, board, 0, 0);
-
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
-                System.out.print(dp[i][j] + " ");
-            }
-            System.out.println();
+        for (int i = 1; i < width; i++) {
+            dp[0][i] = dp[0][i - 1] + board[0][i];
         }
+
+        for (int row = 1; row < height; row++) {
+            rowDp[RIGHT][0] = dp[row - 1][0] + board[row][0];
+
+            for (int col = 1; col < width; col++) {
+                rowDp[RIGHT][col] = Math.max(rowDp[RIGHT][col - 1], dp[row - 1][col]) + board[row][col];
+            }
+
+            rowDp[LEFT][width - 1] = dp[row - 1][width - 1] + board[row][width - 1];
+
+            for (int col = width - 2; col >= 0; col--) {
+                rowDp[LEFT][col] = Math.max(rowDp[LEFT][col + 1], dp[row - 1][col]) + board[row][col];
+            }
+
+            for (int col = 0; col < width; col++) {
+                dp[row][col] = Math.max(rowDp[LEFT][col], rowDp[RIGHT][col]);
+//                System.out.print(rowDp[LEFT][col] + "," + rowDp[RIGHT][col] + " ");
+            }
+//            System.out.println();
+        }
+
+//        for (int i = 0; i < height; i++) {
+//            for (int j = 0; j < width; j++) {
+//                System.out.print(dp[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
 
         return dp[height - 1][width -1];
-    }
-
-    private void dfs(int width, int height, int[][] board, int x, int y) {
-        if (x == width - 1 && y == height - 1) {
-            return;
-        }
-        for (int dir = 0; dir < 3; dir++) {
-            int nx = x + dx[dir];
-            int ny = y + dy[dir];
-
-            if (nx < 0 || nx >= width || ny < 0 || ny >= height) {
-                continue;
-            }
-
-            if (!isVisited[ny][nx] && dp[ny][nx] < dp[y][x] + board[ny][nx]) {
-                isVisited[ny][nx] = true;
-                dp[ny][nx] = dp[y][x] + board[ny][nx];
-                dfs(width, height, board, nx, ny);
-                isVisited[ny][nx] = false;
-            }
-        }
     }
 
     public static void main(String[] args) throws IOException {
