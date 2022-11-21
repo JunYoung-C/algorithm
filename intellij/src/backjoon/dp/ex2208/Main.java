2262ec5cabaa04f59616fd1c18db5c94b7fc28f6
JunYoung-C <doforme524@gmail.com>
2022-11-21 11:04:18 +0900

    wrong: ex2208 보석 줍기

---
+++ intellij/src/backjoon/dp/ex2208/Main.java
@@ -0,0 +1,52 @@
+package backjoon.dp.ex2208;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public int solution(int n, int min, int[] gems) {
+        int answer = Integer.MIN_VALUE;
+        int[] sum = createSumArr(n, gems);
+        int[] dp = new int[n + 1];
+
+        dp[min] = sum[min] - sum[0];
+        for (int i = min + 1; i <= n; i++) {
+            dp[i] = Math.max(dp[i - 1] + gems[i], sum[i] - sum[i - min]);
+        }
+
+        for (int i = min; i <= n; i++) {
+            answer = Math.max(answer, dp[i]);
+        }
+
+        return answer;
+    }
+
+    private int[] createSumArr(int n, int[] gems) {
+        int[] sum = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            sum[i] = sum[i - 1] + gems[i];
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main T = new Main();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int min = Integer.parseInt(st.nextToken());
+        int[] gems = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            gems[i] = Integer.parseInt(br.readLine());
+        }
+
+        br.close();
+        bw.write(T.solution(n, min, gems)+ "");
+        bw.flush();
+        bw.close();
+    }
+}
