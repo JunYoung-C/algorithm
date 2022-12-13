e26907aaf0598e0d5207a4f5756c1cda017dbd87
JunYoung-C <doforme524@gmail.com>
2022-12-13 20:08:45 +0900

    success: ex1753 최단경로

--- intellij/src/backjoon/graph/ex1753/Main.java
+++ intellij/src/backjoon/graph/ex1753/Main.java
@@ -6,47 +6,71 @@ import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-class Node implements Comparable<Node>{
-    int index;
-    int cost;
-
-    public Node(int index, int cost) {
-        this.index = index;
-        this.cost = cost;
-    }
-
-    @Override
-    public int compareTo(Node o) {
-        return this.cost - o.cost;
-    }
-}
-
 public class Main {
-    public int[] solution(int nodeCount, int edgeCount, int startIndex, ArrayList<ArrayList<Node>> graph) {
+    public void solution(int nodeCount, int edgeCount, int startNode, int[][] edges) {
+        ArrayList<ArrayList<Node>> graph = createGraph(nodeCount, edges);
+        int[] distance = getDistance(nodeCount, startNode, graph);
+        for (int i = 1; i <= nodeCount; i++) {
+            if (distance[i] == Integer.MAX_VALUE) {
+                System.out.println("INF");
+            } else {
+                System.out.println(distance[i]);
+            }
+        }
+    }
+
+    private int[] getDistance(int nodeCount, int startNode, ArrayList<ArrayList<Node>> graph) {
         int[] distance = new int[nodeCount + 1];
         Arrays.fill(distance, Integer.MAX_VALUE);
-
         PriorityQueue<Node> pQ = new PriorityQueue<>();
-        pQ.offer(new Node(startIndex, 0));
-        distance[startIndex] = 0;
+        distance[startNode] = 0;
+        pQ.offer(new Node(startNode, 0));
 
         while (!pQ.isEmpty()) {
-            Node currentNode = pQ.poll();
+            Node now = pQ.poll();
 
-            if (distance[currentNode.index] < currentNode.cost) {
+            if (distance[now.index] < now.cost) {
                 continue;
             }
 
-            for (Node next : graph.get(currentNode.index)) {
-                if (distance[next.index] > distance[currentNode.index] + next.cost) {
-                    distance[next.index] = distance[currentNode.index] + next.cost;
-
+            for (Node next : graph.get(now.index)) {
+                if (distance[next.index] > distance[now.index] + next.cost) {
+                    distance[next.index] = distance[now.index] + next.cost;
                     pQ.offer(new Node(next.index, distance[next.index]));
                 }
             }
         }
 
         return distance;
+    }
+
+    private ArrayList<ArrayList<Node>> createGraph(int nodeCount, int[][] edges) {
+        ArrayList<ArrayList<Node>> graph = new ArrayList<>();
+
+        for (int i = 0; i <= nodeCount; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int[] e : edges) {
+            graph.get(e[0]).add(new Node(e[1], e[2]));
+        }
+
+        return graph;
+    }
+
+    class Node implements Comparable<Node> {
+        int index;
+        int cost;
+
+        public Node(int index, int cost) {
+            this.index = index;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return this.cost - o.cost;
+        }
     }
 
     public static void main(String[] args) throws IOException {
@@ -58,33 +82,19 @@ public class Main {
         int nodeCount = Integer.parseInt(st.nextToken());
         int edgeCount = Integer.parseInt(st.nextToken());
 
-        int startIndex = Integer.parseInt(br.readLine());
+        int startNode = Integer.parseInt(br.readLine());
 
-        ArrayList<ArrayList<Node>> graph = new ArrayList<>();
-        for (int i = 0; i <= nodeCount; i++) {
-            graph.add(new ArrayList<>());
-        }
-
+        int[][] edges = new int[edgeCount][3];
         for (int i = 0; i < edgeCount; i++) {
             st = new StringTokenizer(br.readLine());
-
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
 
-            graph.get(a).add(new Node(b, c));
+            edges[i] = new int[] {a, b, c};
         }
 
         br.close();
-        int[] distance = T.solution(nodeCount, edgeCount, startIndex, graph);
-        for (int i = 1; i <= nodeCount; i++) {
-            if (distance[i] == Integer.MAX_VALUE) {
-                bw.write("INF\n");
-            } else {
-                bw.write(distance[i] + "\n");
-            }
-        }
-        bw.flush();
-        bw.close();
+        T.solution(nodeCount, edgeCount, startNode, edges);
     }
 }
